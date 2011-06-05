f83513e806d9f7b2de0fab58480c5b6e06186abe
Bernard Lambeau <blambeau@gmail.com>
2011-06-05 15:59:54 +0200

    Fixed a spec according to grouper ordering

--- spec/grouper_spec.rb
+++ spec/grouper_spec.rb
@@ -16,7 +16,7 @@ class Alf
     subject{ grouper.pipe(input) }
 
     it "should group as expected" do
-      subject.to_a.should == [
+      subject.to_a.sort{|k1,k2| k1[:a] <=> k2[:a]}.should == [
         {:a => "via_method", :as => [{:time => 1}, {:time => 2}]},
         {:a => "via_reader", :as => [{:time => 3}]},
       ]
