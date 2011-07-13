726ab4178dd1b1c3d4fb70140c80d992d2f6b0b4
Bernard Lambeau <blambeau@gmail.com>
2011-07-13 09:28:08 +0200

    Cleaned AlfFile spec test

--- spec/reader/alf_file_spec.rb
+++ spec/reader/alf_file_spec.rb
@@ -2,14 +2,16 @@ require 'spec_helper'
 module Alf
   describe Reader::AlfFile do
     
-    let(:io){ StringIO.new "(restrict :suppliers, lambda{status > 20})" }
-    let(:reader){ Reader::AlfFile.new(io, self) } 
+    let(:io){ StringIO.new(expr) }
     def dataset(name)
       [{:status => 10},{:status => 30}]
     end
-    
-    subject{ reader.to_a }
-    it{ should == [{:status => 30}]}
+    subject{ Reader::AlfFile.new(io, self).to_a }
+
+    describe "on pure functional expressions" do
+      let(:expr){ "(restrict :suppliers, lambda{status > 20})" }
+      it{ should == [{:status => 30}]}
+    end
       
   end
 end
