664a7deb9ce9eb51ac8e9d74f6ab257a38f4302e
Bernard Lambeau <blambeau@gmail.com>
2011-07-13 09:30:53 +0200

    Ensured that AlfFile may be any ruby expression

--- spec/reader/alf_file_spec.rb
+++ spec/reader/alf_file_spec.rb
@@ -12,6 +12,16 @@ module Alf
       let(:expr){ "(restrict :suppliers, lambda{status > 20})" }
       it{ should == [{:status => 30}]}
     end
+
+    describe "on impure functional expressions" do
+      let(:expr){
+      <<-EOF
+        xxx = (restrict :suppliers, lambda{status > 20})
+        (extend xxx, :rev => lambda{ -status })
+      EOF
+      }
+      it{ should == [{:status => 30, :rev => -30}]}
+    end
       
   end
 end
