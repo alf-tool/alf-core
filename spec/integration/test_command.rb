16247f845e659f8f20b1ab03916be23a7aeb101f
Bernard Lambeau <blambeau@gmail.com>
2011-07-14 14:34:04 +0200

    Integration tests must run on Alf::Command::Main directly

--- spec/integration/test_command.rb
+++ spec/integration/test_command.rb
@@ -1,21 +1,31 @@
 require 'spec_helper'
-describe "Alf's alf command" do
+describe "Alf's alf command / " do
 
-  let(:path){ _('../../bin/alf', __FILE__) }
-  
   Dir[_('command/**/*.cmd', __FILE__)].each do |input|
     cmd = File.readlines(input).first
-
     specify{ cmd.should =~ /^alf / }
   
-    describe cmd do
-      let(:args)     { cmd =~ /^alf (.*)/; $1; }
+    describe "#{File.basename(input)}: #{cmd}" do
+      let(:argv)     { cmd =~ /^alf (.*)/; parse_commandline_args($1) }
       let(:stdout)   { File.join(File.dirname(input), "#{File.basename(input, ".cmd")}.stdout") }
       let(:expected) { File.read(stdout).gsub(/\$\(([\S]+)\)/){ Kernel.eval($1) } }
-      specify{ 
-        result = `#{path} #{args}`
-        $?.exitstatus.should == 0 
-        result.should(eq(expected)) unless RUBY_VERSION < "1.9"
+
+      before{ 
+        $oldstdout = $stdout 
+        $stdout = StringIO.new
+      }
+      after { 
+        $stdout = $oldstdout
+        $oldstdout = nil 
+      }
+      
+      specify{
+        begin 
+          Alf::Command::Main.run(argv, __FILE__)
+        rescue SystemExit
+          $stdout << SystemExit << "\n"
+        end
+        $stdout.string.should(eq(expected)) unless RUBY_VERSION < "1.9"
       }
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,5 +11,27 @@ def _(path, file)
   File.expand_path("../#{path}", file)
 end
 
+def parse_commandline_args(args)
+  args = Kernel.eval "%w{#{args}}"
+  result = []
+  until args.empty?
+    if args.first[0,1] == '"'
+      if args.first[-1,1] == '"'
+        result << args.shift[1...-1]
+      else
+        block = [ args.shift[1..-1] ]
+        while args.first[-1,1] != '"'
+          block << args.shift
+        end 
+        block << args.shift[0...-1]
+        result << block.join(" ")
+      end
+    else
+      result << args.shift
+    end  
+  end
+  result
+end
+
 require 'shared/an_operator_class'
 require 'shared/a_value'
