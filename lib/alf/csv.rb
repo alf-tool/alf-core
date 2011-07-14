85cf1e14bf22a8739a2833d2ff5aebb70139a4d5
Bernard Lambeau <blambeau@gmail.com>
2011-07-14 17:13:08 +0200

    Merged CSV files to match Alf's code conventions

--- lib/alf/csv.rb
+++ lib/alf/csv.rb
@@ -1,3 +1,108 @@
-require 'alf/csv/commons'
-require 'alf/csv/renderer'
-require 'alf/csv/reader'
+module Alf
+  module CSV
+    
+    #
+    # Provides common tooling to CSV renderer and readers
+    #
+    module Commons
+
+      private
+      
+      #
+      # Returns the default CSV options
+      #
+      def get_csv_options
+        {:headers => true}
+      end
+      
+      #
+      # Returns CSV in ruby 1.9 and FasterCSV for ruby < 1.9.
+      # This method handles require as well.
+      #
+      def get_csv_class
+        if RUBY_VERSION >= "1.9"
+          require 'csv'
+          ::CSV
+        else
+          ::Alf::Tools::friendly_require('fastercsv')
+          ::FasterCSV
+        end
+      end
+      
+      #
+      # Returns a CSV instance bound to a given io and options
+      #
+      def get_csv(io, options = get_csv_options)
+        get_csv_class.new(io, options)
+      end
+      
+    end # module Commons
+    
+    #
+    # Implements Alf::Renderer contract for outputting CSV files. 
+    #
+    class Renderer < Alf::Renderer
+      include CSV::Commons
+      
+      protected
+      
+      # (see Renderer#render)
+      def render(input, output)
+        csv = get_csv(output)
+        header = nil
+        input.each do |tuple|
+          unless header 
+            header = extract_header(tuple)
+            csv << header.collect{|k| k.to_s}
+          end
+          csv << extract_row(tuple, header)
+        end
+        output
+      end
+      
+      private
+      
+      def extract_header(tuple)
+        tuple.keys
+      end
+      
+      def extract_row(tuple, header)
+        header.collect{|k| tuple[k]}
+      end
+  
+      ::Alf::Renderer.register(:csv, "as a csv file", self)
+    end # class Renderer
+    
+    #
+    # Implements Alf::Reader contract for reading CSV files.
+    #
+    class Reader < Alf::Reader
+      include CSV::Commons
+      
+      def each
+        with_input_io do |io|
+          header = nil
+          block = Proc.new{|row|
+            next if row.header_row?
+            yield(symbolize_keys(row.to_hash))
+          }
+          case io
+          when StringIO
+            get_csv_class.parse(io.string, get_csv_options, &block) 
+          else
+            get_csv(io).each(&block)
+          end
+        end
+      end
+      
+      private
+      
+      def symbolize_keys(h)
+        Hash[h.collect{|k,v| [k.to_sym,v] }]
+      end
+      
+      ::Alf::Reader.register(:csv, [".csv"], self)
+    end # class Reader
+    
+  end # module CSV
+end # module Alf
