4f60c723726da61dce6669b2886b9bcf9f6d0d95
Bernard Lambeau <blambeau@gmail.com>
2011-07-22 16:29:35 +0200

    Ensured no regression when using keywords on restrict

---
+++ spec/regression/restrict/test_restrict_with_keywords.rb
@@ -0,0 +1,17 @@
+require 'spec_helper' 
+module Alf
+  describe "(restrict xxx, :keyword => ...)" do
+    
+    it "should support using a keyword on Relation" do
+      rel = Relation[{:when => "now"}]
+      rel.restrict(:when => "now").should eq(rel)
+    end
+    
+    it "should support using a keyword on Lispy" do
+      Alf.lispy(Environment.examples).evaluate do
+        (restrict (extend :suppliers, {:when => lambda{"notnow"}}), {:when => "now"})
+      end.should eq(Relation::DUM)
+    end
+    
+  end
+end
