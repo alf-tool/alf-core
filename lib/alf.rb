69285fa6afccc7ccd76c28f07c8d0461b4af5495
Bernard Lambeau <blambeau@gmail.com>
2011-07-27 17:28:02 +0200

    Added a Renaming abstraction

--- lib/alf.rb
+++ lib/alf.rb
@@ -385,6 +385,51 @@ module Alf
     end # class OrderingKey
 
     #
+    # Encapsulates a Renaming information 
+    #
+    class Renaming
+      
+      # @return [Hash] a renaming mapping as AttrName -> AttrName
+      attr_reader :renaming     
+      
+      # 
+      # Creates a renaming instance
+      #
+      # @param [Hash] a renaming mapping as AttrName -> AttrName 
+      #
+      def initialize(renaming)
+        @renaming = renaming
+      end 
+      
+      # 
+      # Coerces `arg` to a renaming
+      # 
+      def self.coerce(arg)
+        case arg
+        when Renaming
+          arg
+        when Hash
+          h = Tools.tuple_collect(arg){|k,v|
+            [Tools.coerce(k, AttrName), Tools.coerce(v, AttrName)]
+          }
+          Renaming.new(h)
+        when Array
+          coerce(Hash[*arg])
+        else
+          raise ArgumentError, "Invalid argument `#{arg}` for Renaming()"
+        end
+      end
+      
+      #
+      # Applies renaming to a a given tuple
+      #
+      def apply(tuple)
+        Tools.tuple_collect(tuple){|k,v| [@renaming[k] || k, v]}
+      end
+      
+    end # class Renaming
+    
+    #
     # Encapsulates a Summarization information
     #
     class Summarization
---
+++ spec/unit/tools/test_renaming.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+module Alf
+  module Tools
+    describe Renaming do
+      
+      describe "coerce" do
+        
+        subject{ Renaming.coerce(arg) }
+        
+        describe "from a Renaming" do
+          let(:arg){ Renaming.new(:old => :new) }
+          it{ should eq(arg) }
+        end
+        
+        describe "from a Hash" do
+          let(:arg){ {"old" => "new"} }
+          specify{ subject.renaming.should eq(:old => :new) }
+        end
+        
+        describe "from an Array" do
+          let(:arg){ ["old", "new"] }
+          specify{ subject.renaming.should eq(:old => :new) }
+        end
+          
+      end
+      
+      describe "apply" do
+        let(:r){ Renaming.coerce(["old", "new"])} 
+        specify{
+          r.apply(:old => :a, :other => :b).should eql(:new => :a, :other => :b)
+        }
+      end
+      
+    end 
+  end
+end
