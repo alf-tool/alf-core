cd0ee58c2e6883b9c3edb99afad33989a8e16bae
Bernard Lambeau <blambeau@gmail.com>
2011-07-28 11:30:40 +0200

    Revisited TupleComputation test

--- spec/unit/tools/test_tuple_computation.rb
+++ spec/unit/tools/test_tuple_computation.rb
@@ -3,41 +3,34 @@ module Alf
   module Tools
     describe TupleComputation do
   
-      describe "coerce" do
+      let(:handle){ TupleHandle.new.set(:who => "alf") }
+      subject{ TupleComputation.coerce(arg).compute(handle) }
       
-        subject{ TupleComputation.coerce(arg) }
-        let(:handle){ TupleHandle.new.set(:who => "alf") }
-          
-        describe "from a TupleComputation" do
-          let(:arg){ TupleComputation.new :hello => TupleExpression.coerce(:who) } 
-          it{ should be_a(TupleComputation) }
-          specify{ subject.compute(handle).should eql(:hello => "alf") } 
-        end
-        
-        describe "from a Hash" do
-          let(:arg){ 
-            {:hello  => TupleExpression.coerce(:who),
-             :hello2 => lambda{ who },
-             :hello3 => :who}
-          }
-          let(:expected){
-            {:hello => "alf", :hello2 => "alf", :hello3 => "alf"}
-          }
-          it{ should be_a(TupleComputation) }
-          specify{ subject.compute(handle).should eql(expected) }
-        end
-        
-        describe "from an Array" do
-          let(:arg){ ["hello", "who"] }
-          let(:expected){
-            {:hello => "alf"}
-          }
-          it{ should be_a(TupleComputation) }
-          specify{ subject.compute(handle).should eql(expected) }
-        end
-          
+      describe "from a TupleComputation" do
+        let(:arg){ TupleComputation.new :hello => TupleExpression.coerce(:who) } 
+        it{ should eql(:hello => "alf") } 
       end
-      
+        
+      describe "from a Hash without coercion" do
+        let(:arg){ 
+          {:hello  => TupleExpression.coerce(:who),
+           :hello2 => lambda{ who },
+           :hello3 => :who}
+        }
+        let(:expected){
+          {:hello => "alf", :hello2 => "alf", :hello3 => "alf"}
+        }
+        it{ should eql(expected) }
+      end
+        
+      describe "from an Array with coercions" do
+        let(:arg){ ["hello", "who"] }
+        let(:expected){
+          {:hello => "alf"}
+        }
+        it{ should eql(expected) }
+      end
+          
     end
   end
 end
