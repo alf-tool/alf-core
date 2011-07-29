23eef13e1d311a25aa07a1f2663c1c8d7f7a5258
Bernard Lambeau <blambeau@gmail.com>
2011-07-29 16:15:55 +0200

    Moved TupleComputation in T

--- lib/alf.rb
+++ lib/alf.rb
@@ -115,6 +115,59 @@ module Alf
     
     end # class TupleHandle
     
+    # 
+    # Encapsulates a tuple computation from other tuples expressions
+    #
+    class TupleComputation
+      
+      # @return [Hash] a computation hash, mapping AttrName -> TupleExpression
+      attr_reader :computation
+      
+      #
+      # Creates a TupleComputation instance
+      #
+      # @param [Hash] computation, a mappping AttrName -> TupleExpression
+      #
+      def initialize(computation)
+        @computation = computation
+      end
+      
+      # 
+      # Coerces `arg` to a tuple computation
+      #
+      def self.coerce(arg)
+        case arg
+        when TupleComputation
+          arg
+        when Hash
+          h = Tools.tuple_collect(arg){|k,v|
+            if AttrName === k
+              v = TupleExpression.coerce(v) if v.is_a?(Proc)
+              [k, v] 
+            else
+              [Tools.coerce(k, AttrName), Tools.coerce(v, TupleExpression)]
+            end
+          }
+          TupleComputation.new(h)
+        when Array
+          coerce(Hash[*arg])
+        else
+          raise ArgumentError, "Invalid argument `arg` for TupleComputation()"
+        end
+      end
+      
+      #
+      # Computes the result, given `tuple` as context and `handle` to
+      # evaluate expressions. 
+      #
+      def evaluate(obj = nil)
+        Tools.tuple_collect(@computation){|k,v| 
+          [k, v.is_a?(TupleExpression) ? v.evaluate(obj) : v]
+        }
+      end
+      
+    end # class TupleComputation
+    
     # Install all types on Alf now
     constants(false).each do |s|
       Alf.const_set(s, const_get(s))
@@ -215,59 +268,6 @@ module Alf
       Hash[enum.collect{|elm| yield(elm)}]
     end
 
-    # 
-    # Encapsulates a tuple computation from other tuples expressions
-    #
-    class TupleComputation
-      
-      # @return [Hash] a computation hash, mapping AttrName -> TupleExpression
-      attr_reader :computation
-      
-      #
-      # Creates a TupleComputation instance
-      #
-      # @param [Hash] computation, a mappping AttrName -> TupleExpression
-      #
-      def initialize(computation)
-        @computation = computation
-      end
-      
-      # 
-      # Coerces `arg` to a tuple computation
-      #
-      def self.coerce(arg)
-        case arg
-        when TupleComputation
-          arg
-        when Hash
-          h = Tools.tuple_collect(arg){|k,v|
-            if AttrName === k
-              v = TupleExpression.coerce(v) if v.is_a?(Proc)
-              [k, v] 
-            else
-              [Tools.coerce(k, AttrName), Tools.coerce(v, TupleExpression)]
-            end
-          }
-          TupleComputation.new(h)
-        when Array
-          coerce(Hash[*arg])
-        else
-          raise ArgumentError, "Invalid argument `arg` for TupleComputation()"
-        end
-      end
-      
-      #
-      # Computes the result, given `tuple` as context and `handle` to
-      # evaluate expressions. 
-      #
-      def evaluate(obj = nil)
-        Tools.tuple_collect(@computation){|k,v| 
-          [k, v.is_a?(TupleExpression) ? v.evaluate(obj) : v]
-        }
-      end
-      
-    end # class TupleComputation
-    
     #
     # Defines a projection key
     # 
