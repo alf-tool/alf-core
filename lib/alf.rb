2f3b4c9f445b5509033d84b1b014b3b17748acc4
Bernard Lambeau <blambeau@gmail.com>
2011-08-01 16:28:35 +0200

    Code style

--- lib/alf.rb
+++ lib/alf.rb
@@ -255,12 +255,14 @@ module Alf
   # Marker for all operators, relational and non-relational ones.
   # 
   module Operator
+    include Iterator, Tools
+    
     def Alf.Operator(file, line)
       Alf.Command(file, line) do |b|
         b.instance_module Alf::Operator
       end
     end
-    include Iterator, Tools
+
     require 'alf/operator/class_methods'
     require 'alf/operator/base'
     require 'alf/operator/unary'
@@ -269,64 +271,65 @@ module Alf
     require 'alf/operator/transform'
     require 'alf/operator/shortcut'
     require 'alf/operator/experimental'
-  end # module Operator
-
-  #
-  # Marker module and namespace for non relational operators
-  #
-  module Operator::NonRelational
-    require 'alf/operator/non_relational/autonum'
-    require 'alf/operator/non_relational/defaults'
-    require 'alf/operator/non_relational/compact'
-    require 'alf/operator/non_relational/sort'
-    require 'alf/operator/non_relational/clip'
-    require 'alf/operator/non_relational/coerce'
 
     #
-    # Yields the block with each operator module in turn
+    # Marker module and namespace for non relational operators
     #
-    def self.each
-      constants.each do |c|
-        val = const_get(c)
-        yield(val) if val.ancestors.include?(Operator::NonRelational)
+    module NonRelational
+      require 'alf/operator/non_relational/autonum'
+      require 'alf/operator/non_relational/defaults'
+      require 'alf/operator/non_relational/compact'
+      require 'alf/operator/non_relational/sort'
+      require 'alf/operator/non_relational/clip'
+      require 'alf/operator/non_relational/coerce'
+  
+      #
+      # Yields the block with each operator module in turn
+      #
+      def self.each
+        constants.each do |c|
+          val = const_get(c)
+          yield(val) if val.ancestors.include?(Operator::NonRelational)
+        end
       end
-    end
+      
+    end # NonRelational
     
-  end # Operator::NonRelational
-  
-  #
-  # Marker module and namespace for relational operators
-  #
-  module Operator::Relational
-    require 'alf/operator/relational/project'
-    require 'alf/operator/relational/extend'
-    require 'alf/operator/relational/rename'
-    require 'alf/operator/relational/restrict'
-    require 'alf/operator/relational/join'
-    require 'alf/operator/relational/intersect'
-    require 'alf/operator/relational/minus'
-    require 'alf/operator/relational/union'
-    require 'alf/operator/relational/matching'
-    require 'alf/operator/relational/not_matching'
-    require 'alf/operator/relational/wrap'
-    require 'alf/operator/relational/unwrap'
-    require 'alf/operator/relational/group'
-    require 'alf/operator/relational/ungroup'
-    require 'alf/operator/relational/summarize'
-    require 'alf/operator/relational/rank'
-    require 'alf/operator/relational/quota'
-
-    # 
-    # Yields the block with each operator module in turn
     #
-    def self.each
-      constants.each do |c|
-        val = const_get(c)
-        yield(val) if val.ancestors.include?(Operator::Relational)
-      end
-    end
+    # Marker module and namespace for relational operators
+    #
+    module Relational
+      require 'alf/operator/relational/project'
+      require 'alf/operator/relational/extend'
+      require 'alf/operator/relational/rename'
+      require 'alf/operator/relational/restrict'
+      require 'alf/operator/relational/join'
+      require 'alf/operator/relational/intersect'
+      require 'alf/operator/relational/minus'
+      require 'alf/operator/relational/union'
+      require 'alf/operator/relational/matching'
+      require 'alf/operator/relational/not_matching'
+      require 'alf/operator/relational/wrap'
+      require 'alf/operator/relational/unwrap'
+      require 'alf/operator/relational/group'
+      require 'alf/operator/relational/ungroup'
+      require 'alf/operator/relational/summarize'
+      require 'alf/operator/relational/rank'
+      require 'alf/operator/relational/quota'
   
-  end
+      # 
+      # Yields the block with each operator module in turn
+      #
+      def self.each
+        constants.each do |c|
+          val = const_get(c)
+          yield(val) if val.ancestors.include?(Operator::Relational)
+        end
+      end
+    
+    end # module Relational
+    
+  end # module Operator
 
   #
   # Aggregation operator.
