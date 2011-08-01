5b3a5d368fa5b547dad77dfd18c8aff42b5968a1
Bernard Lambeau <blambeau@gmail.com>
2011-08-01 15:12:32 +0200

    Extracted Types::AttrName

--- lib/alf.rb
+++ lib/alf.rb
@@ -18,30 +18,8 @@ module Alf
   #
   module Types
     require 'alf/types/boolean'
+    require 'alf/types/attr_name'
     
-    # Data type for being a valid attribute name  
-    class AttrName < Symbol
-      extend Myrrha::Domain
-      
-      def self.predicate
-        @predicate ||= lambda{|s| s.to_s =~ /^[a-zA-Z0-9_]+$/}
-      end
-            
-      def self.coerce(arg)
-        if arg.respond_to?(:to_sym)
-          sym = arg.to_sym
-          return sym if self.===(sym) 
-        end
-        raise ArgumentError, "Unable to coerce `#{arg.inspect}` to AttrName"
-      end
-      
-      def self.from_argv(argv, opts = {})
-        raise ArgumentError if argv.size > 1
-        coerce(argv.first || opts[:default]) 
-      end
-      
-    end # class AttrName
-
     #
     # Defines a Heading, that is, a set of attribute (name,domain) pairs.
     #
