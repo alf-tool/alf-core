9492e713e8e60442b47269d6b032fa9dfd03a2a5
Bernard Lambeau <blambeau@gmail.com>
2011-08-01 11:32:20 +0200

    Remove debug info

--- spec/regression/relation/test_relation_allbut_all.rb
+++ spec/regression/relation/test_relation_allbut_all.rb
@@ -4,8 +4,6 @@ module Alf
   describe Relation, "allbut" do
   
     describe "when all attributes are projected away" do
-      before{ puts "--"*20 }
-      after{ puts "--"*20 }
       subject{ 
         Relation[{:sid => "P1"}].allbut([:sid]) 
       }
