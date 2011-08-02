95f38ecf149fce116e7a10ac1d4efb5f9441815c
Bernard Lambeau <blambeau@gmail.com>
2011-08-02 14:05:32 +0200

    Cleaned Join doc

--- lib/alf/operator/relational/join.rb
+++ lib/alf/operator/relational/join.rb
@@ -4,18 +4,18 @@ module Alf
     # Relational join (and cross-join)
     #
     # SYNOPSIS
-    #   #{program_name} #{command_name} [LEFT] RIGHT
     #
-    # API & EXAMPLE
-    #
-    #   (join :suppliers, :parts)
+    #   #{shell_signature}
     #
     # DESCRIPTION
     #
-    # This operator computes the (natural) join of two input iterators. Natural
-    # join means that, unlike what is commonly used in SQL, the default behavior 
-    # is to join on common attributes. You can use the rename operator if this
-    # behavior does not fit your needs.
+    # This operator computes the (natural) join of its operands. 
+    #
+    # Natural join means that, unlike what is commonly used in SQL, the join is 
+    # performed on common attribute names. You can use the rename operator if 
+    # this behavior does not fit your needs.
+    #
+    # EXAMPLE
     #
     #   alf join suppliers supplies 
     #  
