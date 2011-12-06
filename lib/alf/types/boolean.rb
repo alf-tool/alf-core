b63b6745b38c4a29faadd8bab02bc7ac01cd10d3
Bernard Lambeau <blambeau@gmail.com>
2011-12-06 12:02:07 +0100

    Review Boolean and spec it

--- lib/alf/types/boolean.rb
+++ lib/alf/types/boolean.rb
@@ -1,11 +1,24 @@
 module Alf
   module Types
 
+    # Defines the Boolean type which is missing to ruby.
     Boolean = Myrrha::Boolean
 
+    # Converts commandline arguments to a Boolean.
+    #
+    # This method requires `argv` to be either empty or a singleton array.
+    # When empty, `opts[:default]` is used. The value must be coercable
+    # to a Boolean, according to Myrrha::Boolean coercions.
+    #
+    # @param [Array] argv commandline arguments
+    # @params [Hash] opts options (not used)
+    # @raise [ArgumentError] is the coercion fails
     def Boolean.from_argv(argv, opts={})
       raise ArgumentError if argv.size > 1
-      Tools.coerce(argv.first, Boolean)
+      value = argv.first || opts[:default] || false
+      Tools.coerce(value, Boolean)
+    rescue Myrrha::Error
+      raise ArgumentError, "Unable to coerce `#{value.inspect}` to Boolean"
     end
 
   end
