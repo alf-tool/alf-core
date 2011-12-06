d36a847d879f0b3e6a1485d51aa37281bc9077d4
Bernard Lambeau <blambeau@gmail.com>
2011-12-06 11:54:51 +0100

    Enhance AttrName

--- lib/alf/types/attr_name.rb
+++ lib/alf/types/attr_name.rb
@@ -13,10 +13,24 @@ module Alf
 
       class << self
 
+        # The domain predicate
+        PREDICATE = lambda{|s| s.to_s =~ /^[a-zA-Z0-9_]+$/}
+
+        # Returns the domain predicate.
+        #
+        # @return [Proc] the domain predicate as a Proc obbject
         def predicate
-          @predicate ||= lambda{|s| s.to_s =~ /^[a-zA-Z0-9_]+$/}
+          PREDICATE
         end
 
+        # Coerces `arg` to an AttrName.
+        #
+        # All objects that respond to :to_sym and match PREDICATE are
+        # valid coercion arguments.
+        #
+        # @param [Object] arg and argument to coerce to an AttrName
+        # @return [AttrName] an AttrName instance
+        # @raise [ArgumentError] is the coercion fails
         def coerce(arg)
           if arg.respond_to?(:to_sym)
             sym = arg.to_sym
@@ -25,6 +39,15 @@ module Alf
           raise ArgumentError, "Unable to coerce `#{arg.inspect}` to AttrName"
         end
 
+        # Converts commandline arguments to an AttrName.
+        #
+        # This method requires `argv` to be either empty or a singleton array.
+        # When empty, `opts[:default]` is used. The value must be coercable
+        # with `coerce` for this method to suceed.
+        #
+        # @param [Array] argv commandline arguments
+        # @params [Hash] opts options (not used)
+        # @raise [ArgumentError] is the coercion fails
         def from_argv(argv, opts = {})
           raise ArgumentError if argv.size > 1
           coerce(argv.first || opts[:default]) 
