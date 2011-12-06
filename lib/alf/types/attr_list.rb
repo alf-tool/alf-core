58f003f973ba7645c3d7377bf246754f2f9b00cc
Bernard Lambeau <blambeau@gmail.com>
2011-12-06 11:36:55 +0100

    Review AttrList

--- lib/alf/types/attr_list.rb
+++ lib/alf/types/attr_list.rb
@@ -1,17 +1,33 @@
 module Alf
   module Types
     #
-    # Defines a projection key
-    # 
+    # Attribute list.
+    #
+    # An attribute list is an ordered collection of attribute names (see 
+    # AttrName).
+    #
     class AttrList
-    
-      # Projection attributes
+
+      # @return [Array<AttrName>] the list of attribute names
       attr_accessor :attributes
-    
+
+      # Creates an AttrList instance.
+      #
+      # @param [Array<AttrName>] the list of attribute names
       def initialize(attributes)
         @attributes = attributes
       end
-    
+
+      # Coerces `arg` to an AttrList.
+      #
+      # Recognizes coercions are:
+      # - AttrList -> self
+      # - Ordering -> the underlying list of attributes
+      # - Array    -> through AttrName coercion of its elements
+      #
+      # @param [Object] arg the value to coerce to an AttrList
+      # @return [AttrList] an attribute list if coercion succeeds
+      # @raise [ArgumentError] if the coercion fails
       def self.coerce(arg)
         case arg
         when AttrList
@@ -25,18 +41,42 @@ module Alf
         end
       end
 
+      # Builds an AttrList from commandline arguments.
+      #
+      # This method relies on `coerce` and therefore shares its spec.
+      #
+      # @param [Array] argv an array of commandline arguments
+      # @param [Hash] opts convertion options (unused)
+      # @return [AttrList] an attribute list if coercion succeeds
+      # @raise [ArgumentError] if the coercion fails
       def self.from_argv(argv, opts = {})
         coerce(argv)
       end
-          
-      def to_ordering
-        Ordering.new attributes.collect{|arg| [arg, :asc]}
+
+      # Converts this attribute list to an ordering.
+      #
+      # @param [Symbol] :asc or :desc for ordering direction
+      # @return [Ordering] an ordering instance
+      def to_ordering(order = :asc)
+        Ordering.new attributes.map{|arg| [arg, order]}
       end
-    
-      def project(tuple, allbut = false)
-        split(tuple, allbut).first
-      end
-    
+
+      # Splits a tuple in two parts according `allbut`.
+      #
+      # Example:
+      #   list = AttrList.new([:name])
+      #   tuple = {:name => "Jones", :city => "London"}
+      #
+      #   list.split(tuple)
+      #   # => [{:name => "Jones"}, {:city => "London"}]
+      #
+      #   list.split(tuple, true)
+      #   # => [{:city => "London"}, {:name => "Jones"}]
+      #
+      # @param [Hash] tuple a tuple to split
+      # @param [Boolean] allbut unknown attributes as first result?
+      # @return [Array<Hash>] an array containing two tuples, according to known
+      #         attributes and `allbut`
       def split(tuple, allbut = false)
         projection, rest = {}, tuple.dup
         attributes.each do |a|
@@ -46,11 +86,40 @@ module Alf
         allbut ? [rest, projection] : [projection, rest]
       end
 
+      # Projects `tuple` on known (or unknown) attributes.
+      # 
+      # Example:
+      #   list = AttrList.new([:name])
+      #   tuple = {:name => "Jones", :city => "London"}
+      #
+      #   list.project(tuple)
+      #   # => {:name => "Jones"}
+      #
+      #   list.project(tuple, true)
+      #   # => {:city => "London"}
+      #
+      # @param [Hash] tuple a tuple to project
+      # @param [Boolean] allbut projection?
+      # @return Hash the projected tuple
+      def project(tuple, allbut = false)
+        split(tuple, allbut).first
+      end
+
+      # Checks equality with another attribute list.
+      #
+      # @param [Object] other another attribute list
+      # @return [Boolean] true if `other` is an attribute list and contains
+      #         the same attributes, in the same order
       def ==(other)
-        other.is_a?(AttrList) && (other.attributes == attributes)          
+        other.is_a?(AttrList) && (other.attributes == attributes)
       end
       alias :eql? :==
-      
+
+      # Returns an hash code.
+      def hash
+        attributes.hash
+      end
+
     end # class AttrList
   end # module Types
 end # module Alf
--- spec/unit/types/test_attr_list.rb
+++ spec/unit/types/test_attr_list.rb
@@ -3,19 +3,12 @@ module Alf
   describe AttrList do
 
     describe "coerce" do
-      
+
       subject{ AttrList.coerce(arg) }
-      
+
       describe "when passed a AttrList" do
         let(:arg){ [:a, :b] } 
         it{ should eq(AttrList.new(arg)) }
-      end
-      
-      describe "when passed an array" do
-        let(:arg){ [:a, :b] }
-        specify{
-          subject.attributes.should eq([:a, :b])
-        }
       end
 
       describe "when passed an Ordering" do
@@ -23,83 +16,135 @@ module Alf
         it{ should eq(AttrList.new([:a, :b])) }
       end
 
-    end
-    
+      describe "when passed an array" do
+        let(:arg){ [:a, :b] }
+        it{ should eq(AttrList.new([:a, :b])) }
+      end
+
+      describe "when passed an unrecognized argument" do
+        let(:arg){ :not_recognized }
+        specify{
+          lambda{ subject }.should raise_error(ArgumentError)
+        }
+      end
+
+    end # coerce
+
     describe "from_argv" do
-      
+
       subject{ AttrList.from_argv(argv) }
-      
+
       describe "on an empty array" do
         let(:argv){ [] }
         it{ should eq(AttrList.new([])) }
       end
-      
+
       describe "on a singleton" do
         let(:argv){ ["hello"] }
         it{ should eq(AttrList.new([:hello])) }
       end
-      
+
       describe "on multiple strings" do
         let(:argv){ ["hello", "world"] }
         it{ should eq(AttrList.new([:hello, :world])) }
       end
-        
-    end
+
+      describe "when passed an unrecognized argument" do
+        let(:argv){ :not_recognized }
+        specify{
+          lambda{ subject }.should raise_error(ArgumentError)
+        }
+      end
+
+    end # from_argv
 
     describe "to_ordering" do
 
-      specify "when passed an array" do
-        key = AttrList.coerce [:a, :b]
-        okey = key.to_ordering
-        okey.attributes.should == [:a, :b]
-        okey.order_of(:a).should == :asc
-        okey.order_of(:b).should == :asc
+      describe "without direction" do
+        subject{ AttrList.coerce(attrs).to_ordering }
+        let(:attrs){ [:a, :b] }
+        it{ should eq(Ordering.new [[:a, :asc], [:b, :asc]])}
       end
 
-    end
+      describe "with a direction" do
+        subject{ AttrList.coerce(attrs).to_ordering(direction) }
+        let(:attrs){ [:a, :b] }
+        let(:direction){ :desc }
+        it{ should eq(Ordering.new [[:a, :desc], [:b, :desc]])}
+      end
+
+    end # to_ordering
 
     describe "split" do 
 
-      subject{ key.split(tuple, allbut) }
+      let(:key){ AttrList.new [:a, :b] }
+      let(:tuple){ {:a => 1, :b => 2, :c => 3} }
 
       describe "when used without allbut" do
-        let(:key){ AttrList.new [:a, :b] }
-        let(:allbut){ false }
-        let(:tuple){ {:a => 1, :b => 2, :c => 3} }
-        let(:expected){ [{:a => 1, :b => 2}, {:c => 3}] }
-        it{ should == expected }
-      end   
+        subject{ key.split(tuple) }
+        it{ should eq([{:a => 1, :b => 2}, {:c => 3}]) }
+      end
 
-      describe "when used with allbut" do
-        let(:key){ AttrList.new [:a, :b] }
-        let(:allbut){ true }
-        let(:tuple){ {:a => 1, :b => 2, :c => 3} }
-        let(:expected){ [{:c => 3}, {:a => 1, :b => 2}] }
-        it{ should == expected }
-      end   
+      describe "when used with allbut set to true" do
+        subject{ key.split(tuple, true) }
+        it{ should eq([{:c => 3}, {:a => 1, :b => 2}]) }
+      end
 
-    end
+      describe "when used with allbut set to false" do
+        subject{ key.split(tuple, false) }
+        it{ should eq([{:a => 1, :b => 2}, {:c => 3}]) }
+      end
+
+      specify "the documentation example" do
+        list = AttrList.new([:name])
+        tuple = {:name => "Jones", :city => "London"}
+        list.split(tuple).should eq([{:name => "Jones"}, {:city => "London"}])
+        list.split(tuple, true).should eq([{:city => "London"}, {:name => "Jones"}])
+      end
+
+    end # split
 
     describe "project" do 
 
-      subject{ key.project(tuple, allbut) }
+      let(:key){ AttrList.new [:a, :b] }
+      let(:tuple){ {:a => 1, :b => 2, :c => 3} }
 
       describe "when used without allbut" do
-        let(:key){ AttrList.new [:a, :b] }
-        let(:allbut){ false }
-        let(:tuple){ {:a => 1, :b => 2, :c => 3} }
-        let(:expected){ {:a => 1, :b => 2} }
-        it{ should == expected }
-      end   
+        subject{ key.project(tuple) }
+        it{ should eq({:a => 1, :b => 2}) }
+      end
 
-      describe "when used with allbut" do
-        let(:key){ AttrList.new [:a, :b] }
-        let(:allbut){ true }
-        let(:tuple){ {:a => 1, :b => 2, :c => 3} }
-        let(:expected){ {:c => 3} }
-        it{ should == expected }
-      end   
+      describe "when used with allbut set to true" do
+        subject{ key.project(tuple, true) }
+        it{ should eq({:c => 3}) }
+      end
 
+      describe "when used without allbut set to false" do
+        subject{ key.project(tuple, false) }
+        it{ should eq({:a => 1, :b => 2}) }
+      end
+
+      specify "the documentation example" do
+        list = AttrList.new([:name])
+        tuple = {:name => "Jones", :city => "London"}
+        list.project(tuple).should eq({:name => "Jones"})
+        list.project(tuple, true).should eq({:city => "London"})
+      end
+
+    end # project
+
+    it "should define a value" do
+      l1 = AttrList.new [:a, :b]
+      l2 = AttrList.new [:a, :b]
+      l3 = AttrList.new [:a]
+
+      l1.should == l2
+      l1.should eq(l2)
+
+      l1.should_not == l3
+      l1.should_not eq(l3)
+
+      {l1 => 1, l2 => 2}.size.should eq(1)
     end
 
   end
