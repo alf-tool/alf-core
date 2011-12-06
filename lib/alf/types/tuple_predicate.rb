129c7510cea363c6aa047ea004c6d54c2784b5bd
Bernard Lambeau <blambeau@gmail.com>
2011-12-06 18:02:24 +0100

    Review TuplePredicate

--- lib/alf/types/tuple_predicate.rb
+++ lib/alf/types/tuple_predicate.rb
@@ -1,49 +1,84 @@
 module Alf
   module Types
     # 
-    # Specialization of TupleExpression to boolean expressions 
-    # specifically
+    # Specialization of TupleExpression to boolean expressions.
     #
     class TuplePredicate < TupleExpression
-      
-      # 
-      # Coerces `arg` to a TuplePredicate
+
+      class << self
+
+        # Coerces `arg` to a TuplePredicate
+        #
+        # Implemented coercions are:
+        # - TuplePredicate    -> self
+        # - TupleExpression   -> Renewed on this class
+        # - true and false    -> true and false predicates
+        # - Proc              -> TuplePredicate(arg, nil)
+        # - String and Symbol -> as for TupleExpression
+        # - empty Array       -> coerce(true)
+        # - singleton Array   -> coerce(arg.first)
+        # - {"attr" => "value", ...} -> equal predicate with coercions
+        # - {:attr  => ...value..., ...} -> equal predicate without coercions
+        #
+        # @param [Object] arg the value to coerce to a TuplePredicate
+        # @return [TuplePredicate] the predicate if coercion succeeds
+        # @raise [ArgumentError] if coercion fails
+        def coerce(arg)
+          case arg
+          when TuplePredicate
+            arg
+          when TupleExpression
+            TuplePredicate.new arg.expr_lambda, arg.source
+          when TrueClass, FalseClass
+            TuplePredicate.new lambda{ arg }, arg.to_s
+          when Proc
+            TuplePredicate.new arg, nil
+          when String, Symbol
+            TuplePredicate.new eval("lambda{ #{arg} }"), arg
+          when Hash
+            h = Hash[arg.map{|k,v|
+              (AttrName === k) ? 
+                [k,v] : [Tools.coerce(k, AttrName), Kernel.eval(v)]
+            }]
+            source = h.map{|k,v|
+              "(self.#{k} == #{Tools.to_ruby_literal(v)})"
+            }.join(" && ")
+            TuplePredicate.new eval("lambda{ #{source} }"), source
+          when Array
+            (arg.size <= 1) ?
+              coerce(arg.first || true) :
+              coerce(Hash[*arg])
+          else
+            raise ArgumentError, "Invalid argument `#{arg}` for TupleExpression()"
+          end
+        end
+
+        # Convert commandline arguments to a tuple predicate
+        #
+        # This method reuses `coerce(Array)` and therefore shares its spec.
+        #
+        # @param [Array] args commandline arguments
+        # @param [Hash] opts coercion options (not used).
+        # @return [TuplePredicate] the predicate if coercion succeeds
+        # @raise [ArgumentError] if the coercion fails.
+        def from_argv(argv)
+          coerce(argv)
+        end
+
+      end # class << self
+
+      # Returns a ruby literal for this predicate
       #
-      def self.coerce(arg)
-        case arg
-        when TuplePredicate
-          arg
-        when TupleExpression
-          TuplePredicate.new arg.expr_lambda, arg.source
-        when TrueClass, FalseClass
-          TuplePredicate.new lambda{ arg }, arg.to_s
-        when Proc
-          TuplePredicate.new arg, nil
-        when String, Symbol
-          l = eval("lambda{ #{arg} }")
-          TuplePredicate.new l, arg.to_s
-        when Hash
-          h = Tools.tuple_collect(arg){|k,v|
-            (AttrName === k) ? 
-              [k,v] : [Tools.coerce(k, AttrName), Kernel.eval(v)]
-          }
-          source = h.each_pair.collect{|k,v|
-            "(self.#{k} == #{Tools.to_ruby_literal(v)})"
-          }.join(" && ")
-          coerce(source.empty? ? true : source)
-        when Array
-          (arg.size <= 1) ?
-            coerce(arg.first || true) :
-            coerce(Hash[*arg])
+      # @return [String] a literal s.t. `eval(self.to_ruby_literal) == self`
+      def to_ruby_literal
+        if source.nil?
+          raise NotImplementedError, "No known ruby source for this predicate"
         else
-          raise ArgumentError, "Invalid argument `#{arg}` for TupleExpression()"
+          "Alf::TuplePredicate[#{Tools.to_ruby_literal(source)}]"
         end
       end
-      
-      def self.from_argv(argv)
-        coerce(argv)
-      end
-      
+      alias :inspect :to_ruby_literal
+
     end # class TuplePredicate
   end # module Types
 end # module Alf
--- spec/unit/types/test_tuple_predicate.rb
+++ spec/unit/types/test_tuple_predicate.rb
@@ -2,78 +2,90 @@ require 'spec_helper'
 module Alf
   describe TuplePredicate do
 
+    describe "the class itself" do
+      let(:type){ TuplePredicate }
+      def TuplePredicate.exemplars
+        [
+          "status > 10",
+          {:status => 10}
+        ].map{|x| TuplePredicate.coerce(x)}
+      end
+      it_should_behave_like 'A valid type implementation'
+    end
+
     let(:handle){ Tools::TupleHandle.new.set(:status => 10) }
-      
+
     describe "coerce" do
+
       subject{ TuplePredicate.coerce(arg).evaluate(handle) }
-      
+
       describe "from TuplePredicate" do
         let(:arg){ TuplePredicate.new(lambda{ status == 10 }) }
         it{ should eql(true) }
       end
-      
+
       describe "from true" do
         let(:arg){ true }
         it{ should eql(true) }
       end
-      
+
       describe "from false" do
         let(:arg){ false }
         it{ should eql(false) }
       end
-        
+
       describe "from Proc" do
         let(:arg){ lambda{ status == 10 } }
         it{ should eql(true) }
       end
-      
+
       describe "from String" do
         let(:arg){ "status == 10" }
         it{ should eql(true) }
       end
-      
+
       describe "from Symbol" do
         let(:arg){ :status }
         it{ should eql(10) }
       end
-      
+
       describe "from Hash without coercion" do
         let(:arg){ {:status => 10} }
         it{ should eql(true) }
       end
-      
+
       describe "from Hash with coercion" do
         let(:arg){ {"status" => "10"} }
         it{ should eql(true) }
       end
-      
+
       describe "from a singleton Array" do
         let(:arg){ ["status == 10"] }
         it{ should eql(true) }
       end
-      
+
       describe "from an Array with coercion" do
         let(:arg){ ["status", "10"] }
         it{ should eql(true) }
       end
-      
-    end
-    
+
+    end # coerce
+
     describe "from_argv" do
+
       subject{ TuplePredicate.from_argv(argv).evaluate(handle) }
-      
       
       describe "from a singleton Array" do
         let(:argv){ ["status == 10"] }
         it{ should eql(true) }
       end
-      
+
       describe "from an Array with coercion" do
         let(:argv){ ["status", "10"] }
         it{ should eql(true) }
       end
-      
-    end
-     
-  end
-end
+
+    end # from_argv
+
+  end # TuplePredicate
+end # Alf
