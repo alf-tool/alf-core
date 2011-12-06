9dfabaccb499c5e2b0dce0a379f1ff187aa4a839
Bernard Lambeau <blambeau@gmail.com>
2011-12-06 23:55:38 +0100

    Require aggregators in main alf.rb

--- lib/alf.rb
+++ lib/alf.rb
@@ -311,7 +311,15 @@ module Alf
   class Aggregator
     require 'alf/aggregator/class_methods'
     require 'alf/aggregator/instance_methods'
-    require 'alf/aggregator/aggregators'
+    require 'alf/aggregator/count'
+    require 'alf/aggregator/sum'
+    require 'alf/aggregator/min'
+    require 'alf/aggregator/max'
+    require 'alf/aggregator/avg'
+    require 'alf/aggregator/variance'
+    require 'alf/aggregator/stddev'
+    require 'alf/aggregator/collect'
+    require 'alf/aggregator/concat'
 
   end # class Aggregator
   
