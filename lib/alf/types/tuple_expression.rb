34a09171be0196e06bb5af26f2cf030af40abfc0
Bernard Lambeau <blambeau@gmail.com>
2011-12-06 17:22:04 +0100

    Review TupleExpression

--- lib/alf/types/tuple_expression.rb
+++ lib/alf/types/tuple_expression.rb
@@ -1,61 +1,138 @@
 module Alf
   module Types
     #
-    # Encapsulates the notion of tuple expression, which is a Ruby expression
-    # whose evaluates in the context and scope of a specific tuple.
+    # A tuple expression is a Ruby expression whose evaluates in the scope of a 
+    # specific tuple handle.
+    #
+    # Example:
+    #   expr = TupleExpression["status * 10"]
+    #   expr.call(:status => 20)
+    #   # => 200
     #
     class TupleExpression
-      
+
       # @return [Proc] the lambda expression
       attr_reader :expr_lambda
-      
-      # @return [String] the expression source code 
+
+      # @return [String] the expression source code (may be nil)
       attr_reader :source
-      
-      #
+
       # Creates a tuple expression from a Proc object
       #
       # @param [Proc] expr a Proc for the expression 
-      #
+      # @param [String] source the source code of the expression (optional)
       def initialize(expr, source = nil)
         @expr_lambda = expr
         @source = source
       end
-      
-      # 
-      # Coerces `arg` to a tuple expression
-      # 
-      def self.coerce(arg)
-        case arg
-        when TupleExpression
-          arg
-        when Proc
-          TupleExpression.new(arg, nil)
-        when String, Symbol
-          lamb = eval("lambda{ #{arg} }")
-          TupleExpression.new(lamb, arg.to_s)
-        else
-          raise ArgumentError, "Invalid argument `#{arg}` for TupleExpression()"
+
+      class << self
+
+        # Coerces `arg` to a tuple expression.
+        #
+        # Implemented coercions are:
+        # - TupleExpression -> self
+        # - Proc            -> TupleExpression.new(arg, nil)
+        # - String          -> TupleExpression.new(..., arg)
+        # - Symbol          -> TupleExpression.new(..., arg)
+        #
+        # @param [Object] arg a value to convert to a tuple expression
+        # @return [TupleExpression] the expression if coercion succeeds
+        # @raise [ArgumentError] if the coercion fails
+        def coerce(arg)
+          case arg
+          when TupleExpression
+            arg
+          when Proc
+            TupleExpression.new(arg, nil)
+          when String, Symbol
+            TupleExpression.new(eval("lambda{ #{arg} }"), arg)
+          else
+            raise ArgumentError, "Invalid argument `#{arg}` for TupleExpression()"
+          end
         end
-      end
+        alias :[] :coerce
 
-      # Coerces from ARGV 
-      def self.from_argv(argv, options = {})
-        raise ArgumentError if argv.size > 1
-        coerce(argv.first || options[:default])
-      end
+        # Convert commandline arguments to a tuple expression
+        #
+        # Arguments should either be empty or a singleton. In the former case,
+        # `opts[:default]` is used. In the latter case, the unique argument is
+        # taken as the expression source code and `coerce` is called on it.
+        #
+        # @param [Array] args commandline arguments
+        # @param [Hash] opts coercion options.
+        # @return [TupleExpression] the expression if coercion succeeds
+        # @raise [ArgumentError] if the coercion fails.
+        def from_argv(argv, opts = {})
+          raise ArgumentError if argv.size > 1
+          coerce(argv.first || opts[:default])
+        end
 
+      end # class << self
+
+      # Evaluates the expression in the context of a TupleHandle
       #
-      # Evaluates in the context of obj
-      #
-      def evaluate(obj = nil)
+      # @param [TupleHandle] handle a tuple handle instance.
+      # @return [Object] the result of evaluating the expression in the context
+      #         of `handle`
+      def evaluate(handle = nil)
         if RUBY_VERSION < "1.9"
-          obj.instance_eval(&@expr_lambda)
+          handle.instance_eval(&@expr_lambda)
         else
-          obj.instance_exec(&@expr_lambda)
+          handle.instance_exec(&@expr_lambda)
         end
       end
-      
+
+      # Evaluates the expression on a tuple
+      #
+      # This is a convenient method for the following, longer expression:
+      #
+      #     expr.evaluate(TupleHandle.new.set(tuple))
+      # 
+      # Note, however, using a Handle as in the the expression above is much
+      # more efficient when evaluating the same expression on multiple tuples
+      # in sequence.
+      #
+      # @param [Hash] tuple a Tuple instance
+      # @return [Object] the result of evaluating the expression on `tuple`
+      def call(tuple)
+        evaluate(Tools::TupleHandle.new.set(tuple))
+      end
+      alias :[] :call
+
+      # Returns a hash code.
+      #
+      # @return [Integer] a hash code for this expression
+      def hash
+        @source.nil? ? @expr_lambda.hash : @source.hash
+      end
+
+      # Checks equality with another expression
+      #
+      # @param [TupleExpression] another expression.
+      # @return [Boolean] true if self and other are equal, false otherwise
+      def ==(other)
+        return false unless other.is_a?(TupleExpression)
+        if source.nil? || other.source.nil?
+          expr_lambda == other.expr_lambda
+        else
+          source == other.source
+        end
+      end
+      alias :eql? :==
+
+      # Returns a ruby literal for this expression.
+      #
+      # @return [String] a literal s.t. `eval(self.to_ruby_literal) == self`
+      def to_ruby_literal
+        if source.nil?
+          raise NotImplementedError, "No known ruby source for this expression"
+        else
+          "Alf::TupleExpression[#{Tools.to_ruby_literal(source)}]"
+        end
+      end
+      alias :inspect :to_ruby_literal
+
     end # class TupleExpression
   end # module Types
 end # module Alf
--- spec/unit/types/test_tuple_expression.rb
+++ spec/unit/types/test_tuple_expression.rb
@@ -2,19 +2,35 @@ require 'spec_helper'
 module Alf
   describe TupleExpression do
 
+    describe "the class itself" do
+      let(:type){ TupleExpression }
+      def TupleExpression.exemplars
+        [
+          "10",
+          "status > 10",
+        ].map{|x| TupleExpression.coerce(x)}
+      end
+      it_should_behave_like 'A valid type implementation'
+    end
+
+    it "should include a valid example" do
+      expr = TupleExpression["status * 10"]
+      expr.call(:status => 20).should eq(200)
+    end
+
     let(:handle) {
       Tools::TupleHandle.new.set(:status => 10)
     }
-    
+
     describe "coerce" do
-      
+
       subject{ TupleExpression.coerce(arg) }
-        
+
       describe "with nil" do
         let(:arg){ nil }
-        specify{ lambda{subject}.should raise_error(ArgumentError) }
+        specify{ lambda{ subject }.should raise_error(ArgumentError) }
       end
-      
+
       describe "with a String" do
         let(:arg){ "true" }
         it { should be_a(TupleExpression) }
@@ -23,16 +39,16 @@ module Alf
           subject.source.should eq("true")
         }
       end
-      
+
       describe "with a Symbol" do
         let(:arg){ :status }
         it { should be_a(TupleExpression) }
         specify{ 
           subject.evaluate(handle).should eql(10) 
-          subject.source.should eq("status")
+          subject.source.should eq(:status)
         }
       end
-      
+
       describe "with a Proc" do
         let(:arg){ lambda{ :hello } }
         it { should be_a(TupleExpression) }
@@ -41,24 +57,52 @@ module Alf
           subject.source.should be_nil
         }
       end
-      
-    end
-    
+
+    end # coerce
+
     describe "from_argv" do
-      
-      subject{ TupleExpression.from_argv(argv).evaluate(handle) }
-        
-      describe "with a String" do
+
+      subject{ TupleExpression.from_argv(argv) }
+
+      describe "with a String (1)" do
         let(:argv){ %w{true} }
-        it{ should eql(true) }
+        it { should be_a(TupleExpression) }
+        specify{ 
+          subject.evaluate(handle).should eql(true) 
+          subject.source.should eq("true")
+        }
       end
-        
+
+      describe "with a String (2)" do
+        let(:argv){ ["status > 10"] }
+        it { should be_a(TupleExpression) }
+        specify{ 
+          subject.evaluate(handle).should eql(false) 
+          subject.source.should eq("status > 10")
+        }
+      end
+
       describe "with two String" do
         let(:argv){ %w{hello world} }
         specify{ lambda{subject}.should raise_error(ArgumentError) }
       end
-      
+
+    end # from_argv
+
+    describe 'call' do
+      let(:expr){ TupleExpression["status > 10"] }
+
+      it 'should build its handle correctly' do
+        expr.call(:status => 20).should be_true
+        expr.call(:status => 5).should be_false
+      end
+
+      it 'should be aliased ad []' do
+        expr[:status => 20].should be_true
+        expr[:status => 5].should be_false
+      end
+
     end
-      
-  end   
-end
+
+  end # TupleExpression
+end # Alf
