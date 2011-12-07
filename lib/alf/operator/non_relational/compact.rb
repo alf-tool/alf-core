8d817348bdedaf92744a236686262c89239d7f5e
Bernard Lambeau <blambeau@gmail.com>
2011-12-07 16:12:23 +0100

    Compact operator uses Engine::Compact

--- lib/alf/operator/non_relational/compact.rb
+++ lib/alf/operator/non_relational/compact.rb
@@ -1,62 +1,15 @@
 module Alf
   module Operator::NonRelational
     class Compact < Alf::Operator()
-      include Operator::NonRelational, Operator::Shortcut, Operator::Unary
-  
+      include NonRelational, Unary
+
       signature do |s|
       end
-      
-      # Removes duplicates according to a complete order
-      class SortBased
-        include Operator, Operator::Cesure
 
-        def initialize
-          @cesure_key ||= AttrList.new([])
-        end
-          
-        protected
-        
-        # (see Operator::Cesure#project)
-        def project(tuple)
-          @cesure_key.project_tuple(tuple, true)
-        end
-  
-        # (see Operator::Cesure#accumulate_cesure)
-        def accumulate_cesure(tuple, receiver)
-          @tuple = tuple
-        end
-  
-        # (see Operator::Cesure#flush_cesure)
-        def flush_cesure(key, receiver)
-          receiver.call(@tuple)
-        end
- 
-      end # class SortBased
-  
-      # Removes duplicates by loading all in memory and filtering 
-      # them there 
-      class BufferBased
-        include Operator, Operator::Unary
-  
-        protected
-        
-        def _prepare
-          @tuples = input.to_a.uniq
-        end
-  
-        def _each
-          @tuples.each(&Proc.new)
-        end
-  
-      end # class BufferBased
-  
-      protected 
-      
-      def longexpr
-        chain BufferBased.new,
-              datasets
+      def each(&block)
+        Engine::Compact.new(input).each(&block)
       end
-  
+
     end # class Compact
   end # module Operator::NonRelational
 end # module Alf
