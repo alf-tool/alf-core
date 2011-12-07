30d344dd64c569d631125dba4002ad773884ec31
Bernard Lambeau <blambeau@gmail.com>
2011-12-07 17:52:47 +0100

    Restrict operator uses Filter

--- lib/alf/operator/relational/restrict.rb
+++ lib/alf/operator/relational/restrict.rb
@@ -1,20 +1,16 @@
 module Alf
   module Operator::Relational
     class Restrict < Alf::Operator()
-      include Operator::Relational, Operator::Unary
-      
+      include Relational, Unary
+
       signature do |s|
         s.argument :predicate, TuplePredicate, "true"
       end
-      
-      protected 
-    
-      # (see Operator#_each)
-      def _each
-        handle = TupleHandle.new
-        each_input_tuple{|t| yield(t) if @predicate.evaluate(handle.set(t)) }
+
+      def each(&block)
+        Engine::Filter.new(input, predicate).each(&block)
       end
-  
+
     end # class Restrict
   end # module Operator::Relational
 end # module Alf
