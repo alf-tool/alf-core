2be9922c0249dbe9c8e7c5f71f5bbc4321bef3e4
Bernard Lambeau <blambeau@gmail.com>
2011-12-07 16:29:06 +0100

    Defaults operator uses Engine::Defaults

--- lib/alf/operator/non_relational/defaults.rb
+++ lib/alf/operator/non_relational/defaults.rb
@@ -1,25 +1,19 @@
 module Alf
   module Operator::NonRelational
     class Defaults < Alf::Operator()
-      include Operator::NonRelational, Operator::Transform
-  
+      include NonRelational, Unary
+
       signature do |s|
         s.argument :defaults, TupleComputation, {}
         s.option   :strict,   Boolean, false, "Restrict to default attributes only?"
       end
-      
-      protected 
-  
-      # (see Operator::Transform#_tuple2tuple)
-      def _tuple2tuple(tuple)
-        handle = TupleHandle.new.set(tuple)
-        defs = @defaults.evaluate(handle)
-        keys = @strict ? defs.keys : (tuple.keys | defs.keys)
-        tuple_collect(keys){|k|
-          [k, coalesce(tuple[k], defs[k])]
-        }
+
+      def each(&block)
+        op = Engine::Defaults.new(input, defaults)
+        op = Engine::Clip.new(op, defaults.to_attr_list, false) if strict
+        op.each(&block)
       end
-      
+
     end # class Defaults
   end # module Operator::NonRelational
 end # module Alf
