0337c82723d98fcbf0812581dc6dc1495925d6f3
Bernard Lambeau <blambeau@gmail.com>
2011-12-07 10:33:39 +0100

    Enhance documentation of Tools.to_ruby_literal

--- lib/alf/tools/to_ruby_literal.rb
+++ lib/alf/tools/to_ruby_literal.rb
@@ -4,11 +4,28 @@ module Alf
     # Myrrha rules for converting to ruby literals
     ToRubyLiteral = Myrrha::ToRubyLiteral.dup.append do
     end
-    
-    # Delegated to ToRubyLiteral
+
+    # Converts `value` to a ruby literal
+    #
+    # This method is provided for code generation mechanisms used by Alf in
+    # various places (such as .rash files). The to_ruby_literal contract is
+    # such that the following invariant holds:
+    #
+    #     eval(to_ruby_literal(value)) == value
+    #
+    # This contract is ensured by Myrrha::ToRubyLiteral for various ruby values.
+    # Myrrha delegates the job to `value.to_ruby_literal` provided this method 
+    # exists. In such case, the implementation must be such that the invariant
+    # above is met.
+    #
+    # In every case, an invocation to this method only makes sense provided that
+    # `value` denotes a pure value, with the obvious semantics (from TTM).
+    #
+    # @param [Object] value any ruby object that denotes a pure value.
+    # @return [String] a ruby literal for `value`
     def to_ruby_literal(value)
       ToRubyLiteral.apply(value)
     end
-    
+
   end # module Tools
 end # module Alf
--- spec/unit/tools/test_to_ruby_literal.rb
+++ spec/unit/tools/test_to_ruby_literal.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 module Alf
   describe "Tools#to_ruby_literal" do
-    
-    specify {
+
+    it 'should work on Integers' do
       Tools.to_ruby_literal(12).should eql("12")
-    }
-    
+    end
+
   end
 end
