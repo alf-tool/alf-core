a6fffb0fc7eddec0d2e85a7fca80d420b49cf94b
Bernard Lambeau <blambeau@gmail.com>
2011-12-07 16:09:35 +0100

    Clip operator uses Engine::Clip

--- lib/alf/operator/non_relational/clip.rb
+++ lib/alf/operator/non_relational/clip.rb
@@ -1,20 +1,17 @@
 module Alf
   module Operator::NonRelational
     class Clip < Alf::Operator()
-      include Operator::NonRelational, Operator::Transform
-  
+      include NonRelational, Unary
+
       signature do |s|
         s.argument :attributes, AttrList, []
         s.option   :allbut,     Boolean, false, "Apply an allbut clipping?"
       end
-      
-      protected 
-  
-      # (see Operator::Transform#_tuple2tuple)
-      def _tuple2tuple(tuple)
-        @attributes.project_tuple(tuple, @allbut)
+
+      def each(&block)
+        Engine::Clip.new(input, attributes, allbut).each(&block)
       end
-  
+
     end # class Clip
   end # module Operator::NonRelational
 end # module Alf
