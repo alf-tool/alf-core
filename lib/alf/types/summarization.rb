81dc489ac273604893a84d96cebe54d4324dd541
Bernard Lambeau <blambeau@gmail.com>
2011-12-07 18:41:50 +0100

    Review Summarization

--- lib/alf/types/summarization.rb
+++ lib/alf/types/summarization.rb
@@ -1,76 +1,124 @@
 module Alf
   module Types
     #
-    # Encapsulates a Summarization information
+    # Encapsulates a Summarization information.
     #
     class Summarization
-      
+
       # @return [Hash] the hash of aggregations, AttrName -> Aggregators
       attr_reader :aggregations
-      
-      #
+
       # Creates a Summarization instance
       #
       # @param [Hash] aggs, aggregations as a mapping AttrName -> Aggregators
-      #
       def initialize(aggs)
         @aggregations = aggs
       end
-      
-      #
-      # Coerces `arg` to an Aggregator
-      #
-      def self.coerce(arg)
-        case arg
-        when Summarization
-          arg
-        when Array
-          coerce(Hash[*arg])
-        when Hash
-          h = Tools.tuple_collect(arg) do |k,v|
-            [Tools.coerce(k, AttrName), Tools.coerce(v, Aggregator)]
+
+      class << self
+
+        # Coerces `arg` to an Summarization
+        #
+        # Implemented coercions are:
+        # - Summarization        -> self
+        # - [attr1, agg1, ...]   -> {AttrName(attr1) -> Aggregator(agg1), ...}
+        # - {attr1 => agg1, ...} -> {AttrName(attr1) -> Aggregator(agg1), ...}
+        # 
+        # @param [Object] arg any ruby object to coerce to an Summarization
+        # @return [Summarization] the coerced summarization
+        # @raise [ArgumentError] is the coercion fails
+        def coerce(arg)
+          case arg
+          when Summarization
+            arg
+          when Array
+            coerce(Hash[*arg])
+          when Hash
+            Summarization.new Hash[arg.map{|k,v|
+              [ Tools.coerce(k, AttrName), 
+                Tools.coerce(v, Aggregator) ]
+            }]
+          else
+            raise ArgumentError, "Invalid arg `#{arg}` for Summarization()"
           end
-          Summarization.new(h)
-        else
-          raise ArgumentError, "Invalid arg `#{arg}` for Summarization()"
         end
+        alias :[] :coerce
+
+        # Coerces commandline arguments to a Summarization
+        #
+        # This method reuses `coerce(Array)` and therefore shares its spec.
+        #
+        # @params [Array<String>] argv commandline arguments
+        # @params [Hash] opts coercion options (not used)
+        # @return [Summarization] the coerced summarization
+        # @raise [ArgumentError] is the coercion fails
+        def from_argv(argv, opts = {})
+          coerce(argv)
+        end
+
       end
-      
-      def self.from_argv(argv, opts = {})
-        coerce(argv)
-      end
-      
+
+      # Computes the least tuple.
       #
-      # Computes the least tuple
-      #
+      # @return [Tuple] a tuple with least values for each attribute
       def least
-        Tools.tuple_collect(@aggregations){|k,v| 
+        Hash[@aggregations.map{|k,v| 
           [k, v.least]
-        }
+        }]
       end
-      
+
+      # Computes the resulting aggregation from aggs if tuple happens.
       #
-      # Computes the resulting aggregation from aggs if tuple happens. 
-      #
+      # @return [Tuple] the new aggregated tuple
       def happens(aggs, tuple)
-        Tools.tuple_collect(@aggregations){|k,v|
+        Hash[@aggregations.map{|k,v|
           [k, v.happens(aggs[k], tuple)]
-        }
+        }]
       end
-      
+
+      # Finalizes the summarization
       #
-      # Finalizes the summarization `aggs` 
-      #
+      # @return [Tuple] the finalized aggregated tuple
       def finalize(aggs)
-        Tools.tuple_collect(@aggregations){|k,v|
+        Hash[@aggregations.map{|k,v|
           [k, v.finalize(aggs[k])]
-        }
+        }]
       end
-      
+
+      # Returns a hash code.
+      #
+      # @return [Integer] a hash code for this expression
+      def hash
+        aggregations.hash
+      end
+
+      # Checks equality with another summarization
+      #
+      # @param [Summarization] another summariation
+      # @return [Boolean] true is self and other are equal, false otherwise
       def ==(other)
         other.is_a?(Summarization) && (other.aggregations == aggregations)
       end
-      
+      alias :eql? :==
+
+      # Converts to an attribute list.
+      #
+      # @return [AttrList] a list of computed attribute names
+      def to_attr_list
+        AttrList.new(aggregations.keys)
+      end
+
+      # Returns a ruby literal for this expression.
+      #
+      # @return [String] a literal s.t. `eval(self.to_ruby_literal) == self`
+      def to_ruby_literal
+        map = Hash[aggregations.map{|k,v| 
+          [k.to_s, "#{v.has_source_code!}"]
+        }]
+        "Alf::Summarization[#{Tools.to_ruby_literal(map)}]"
+      end
+      alias :inspect :to_ruby_literal
+
     end # class Summarization
   end # module Types
 end # module Alf
--- spec/unit/alf-core/types/test_summarization.rb
+++ spec/unit/alf-core/types/test_summarization.rb
@@ -1,16 +1,26 @@
 require 'spec_helper'
 module Alf
   describe Summarization do
-    
+
+    describe "the class itself" do
+      let(:type){ Summarization }
+      def Summarization.exemplars
+        [
+          ["total", "sum{ qty }"]
+        ].map{|x| Summarization.coerce(x)}
+      end
+      it_should_behave_like 'A valid type implementation'
+    end
+
     describe "coerce" do
-    
+
       subject{ Summarization.coerce(arg) }
-      
+
       describe "from a Summarization" do
         let(:arg){ Summarization.new(:s => Aggregator.sum{ qty }) }
         it{ should eq(arg) }
       end
-      
+
       describe "from a Hash" do
         let(:arg){ { :s => "sum{ qty }", :m => Aggregator.max{ size } } }
         it{ should be_a(Summarization) }
@@ -20,7 +30,7 @@ module Alf
           }.should be_true
         }
       end
-      
+
       describe "from an Array" do
         let(:arg){ ["s", "sum{ qty }", "m", "max{ size }"] }
         it{ should be_a(Summarization) }
@@ -31,13 +41,13 @@ module Alf
           }.should be_true
         }
       end
-        
-    end
-    
+
+    end # coerce
+
     describe "from_argv" do
-    
+
       subject{ Summarization.from_argv(argv) }
-      
+
       describe "from an Array" do
         let(:argv){ ["s", "sum{ qty }", "m", "max{ size }"] }
         it{ should be_a(Summarization) }
@@ -48,9 +58,9 @@ module Alf
           }.should be_true
         }
       end
-        
-    end
-    
+
+    end # from argv
+
     specify "least -> happens -> finalize" do
       summ = Summarization.coerce(["s", "sum{ qty }", "m", "max{ size }"])
       (x = summ.least).should eql(:s => 0, :m => nil)
@@ -58,6 +68,6 @@ module Alf
       (x = summ.happens(x, :qty => 5, :size => 5)).should eq(:s => 15, :m => 12)
       summ.finalize(x).should eq(:s => 15, :m => 12)
     end
-    
+
   end
 end
