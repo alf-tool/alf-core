f77f00b08728538bb8675b252ce5c75f7ff40a16
Bernard Lambeau <blambeau@gmail.com>
2011-12-07 09:16:08 +0100

    Tools in its own file.

--- lib/alf.rb
+++ lib/alf.rb
@@ -14,20 +14,7 @@ require 'myrrha/coerce'
 #
 module Alf
   require 'alf/types'
-
-  #
-  # Provides tooling methods that are used here and there in Alf.
-  # 
-  module Tools
-    require 'alf/tools/coerce'
-    require 'alf/tools/to_ruby_literal'
-    require 'alf/tools/to_lispy'
-    require 'alf/tools/tuple_handle'
-    require 'alf/tools/miscellaneous'
-
-    extend Tools
-  end # module Tools
-
+  require 'alf/tools'
   require 'alf/environment'
 
   #
