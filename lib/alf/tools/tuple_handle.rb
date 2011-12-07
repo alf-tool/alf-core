b77127efbbfecd7b1802ddd756ba3804bb17be6b
Bernard Lambeau <blambeau@gmail.com>
2011-12-07 11:17:22 +0100

    Enhance doc of TupleHandle

--- lib/alf/tools/tuple_handle.rb
+++ lib/alf/tools/tuple_handle.rb
@@ -1,42 +1,52 @@
 module Alf
   module Tools
     #
-    # Provides a handle, implementing a flyweight design pattern on tuples.
+    # Provides a handle for implementing a flyweight design pattern on tuples.
+    #
+    # This class works hand-in-hand with TupleExpression as it provides a scope
+    # for their evaluation on tuples.
+    #
+    # Example:
+    #
+    #   handle = TupleHandle.new
+    #   expr   = TupleExpression["status > 10"]
+    #   relation.each do |tuple|
+    #     expr.evaluate(handle.set(tuple))
+    #   end
     #
     class TupleHandle
-    
-      # Creates an handle instance
+
+      # Creates a handle instance
       def initialize
         @tuple = nil
       end
-    
-      #
+
       # Sets the next tuple to use.
       #
-      # This method installs the handle as a side effect 
-      # on first call. 
+      # On first call, this method installs the handle as a side effect.
       #
+      # @param [Tuple] tuple the current iterated tuple
+      # @return [TupleHandle] self
       def set(tuple)
         build(tuple) if @tuple.nil?
         @tuple = tuple
         self
       end
-    
-      #
+
       # Evaluates a tuple expression on the current tuple.
       # 
+      # @param [Object] expr a tuple expression (coercions apply)
+      # @return [Object] the result of evaluting `expr` on self
       def evaluate(expr)
         TupleExpression.coerce(expr).evaluate(self)
       end
-    
+
       private
-    
-      #
+
       # Builds this handle with a tuple.
       #
-      # This method should be called only once and installs 
-      # instance methods on the handle with keys of _tuple_.
-      #
+      # This method should be called only once and installs instance methods on 
+      # the handle with keys of _tuple_.
       def build(tuple)
         tuple.keys.each do |k|
           (class << self; self; end).send(:define_method, k) do
@@ -44,7 +54,7 @@ module Alf
           end
         end
       end
-    
+
     end # class TupleHandle
   end # module Tools
 end # module Alf
--- spec/unit/tools/test_tuple_handle.rb
+++ spec/unit/tools/test_tuple_handle.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 module Alf
   module Tools
     describe TupleHandle do
-  
+
       let(:handle){ TupleHandle.new }
-  
+
       it "should install methods properly" do
         handle.set(:hello => "a", :world => "b")
         handle.should respond_to(:hello)
         handle.should respond_to(:world)
       end
-  
+
       it "should behave correctly" do
         handle.set(:hello => "a", :world => "b")
         handle.hello.should == "a"
@@ -19,34 +19,34 @@ module Alf
         handle.hello.should == "c"
         handle.world.should == "d"
       end
-  
+
       it "should allow instance evaluating on exprs" do
         handle.set(:tested => 1)
         handle.instance_eval{ tested < 1 }.should be_false
       end
-  
+
       it "should support an attribute called :path" do
         handle.set(:path => 1)
         handle.instance_eval{ path < 1 }.should be_false
       end
-    
+
       describe "evaluate" do
         before{ handle.set(:a => 1, :b => 2) }
-        
+
         it "should allow a String" do
           handle.evaluate("a").should == 1
         end
-        
+
         it "should allow a Symbol" do
           handle.evaluate(:a).should == 1
         end
-        
+
         it "should allow a Proc" do
           handle.evaluate(lambda{ a }).should == 1
         end
-        
-      end
-  
-    end   
+
+      end # evaluate
+
+    end
   end
 end
