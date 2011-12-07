802a3f337e855065b30d68a03f3fe664c34d16e5
Bernard Lambeau <blambeau@gmail.com>
2011-12-07 14:52:26 +0100

    Set-based heuristics to compact

--- lib/alf-engine/alf/engine/compact/set.rb
+++ lib/alf-engine/alf/engine/compact/set.rb
@@ -1,26 +1,23 @@
 module Alf
   module Engine
     #
-    # Remove duplicate tuples from its operand.
+    # Remove duplicate tuples through an in-memory `to_set` heuristics.
     #
-    class Compact < Cog
+    class Compact::Set < Cog
 
       # @return [Enumerable] The operand
       attr_reader :operand
 
-      # Creates a Compact instance
+      # Creates a Compact::Set instance
       def initialize(operand)
         @operand = operand
       end
 
       # (see Cog#each)
       def each(&block)
-        Compact::Uniq.new(operand).each(&block)
+        operand.to_set.each(&block)
       end
 
-    end # class Compact
+    end # class Compact::Set
   end # module Engine
 end # module Alf
-require 'alf/engine/compact/uniq'
-require 'alf/engine/compact/set'
-
---
+++ spec/unit/alf-engine/compact/test_set.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+module Alf
+  module Engine
+    describe Compact::Set do
+
+      it 'should work on an empty operand' do
+        Compact::Set.new([]).to_a.should eq([])
+      end
+
+      it 'should work when no duplicate is present' do
+        rel = [
+          {:name => "Jones"},
+          {:name => "Smith"}
+        ]
+        Compact::Set.new(rel).to_set.should eq(rel.to_set)
+      end
+
+      it 'should work when duplicates are present' do
+        rel = [
+          {:name => "Jones"},
+          {:name => "Smith"},
+          {:name => "Jones"},
+        ]
+        exp = [
+          {:name => "Jones"},
+          {:name => "Smith"}
+        ]
+        Compact::Set.new(rel).to_set.should eq(exp.to_set)
+      end
+
+    end
+  end # module Engine
+end # module Alf
