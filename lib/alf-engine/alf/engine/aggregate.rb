c0c928e190d7d53229e66c63677c770d3db4163a
Bernard Lambeau <blambeau@gmail.com>
2011-12-07 18:51:40 +0100

    Add Engine::Aggregate

---
+++ lib/alf-engine/alf/engine/aggregate.rb
@@ -0,0 +1,44 @@
+module Alf
+  module Engine
+    #
+    # Aggregates the whole operand according to a Summarization. The resulting
+    # iterator contains only one tuple.
+    #
+    # Example:
+    #
+    #   res = [
+    #     {:name => "Jones", :price => 12.0, :id => 1},
+    #     {:name => "Smith", :price => 10.0, :id => 2}
+    #   ]
+    #   agg = Summarization[:size  => "count", 
+    #                       :total => "sum{ price }"]
+    #   Aggregate.new(res, agg).to_a
+    #   # => [
+    #   #      {:size => 2, :total => 22.0}
+    #   #    ]
+    #
+    class Aggregate < Cog
+
+      # @return [Enumerable] The operand
+      attr_reader :operand
+
+      # @return [Summarization] The summarization to use
+      attr_reader :summarization
+
+      # Creates an Aggregate instance
+      def initialize(operand, summarization)
+        @operand = operand
+        @summarization = summarization
+      end
+
+      # (see Cog#each)
+      def each
+        agg = operand.inject(@summarization.least) do |memo,tuple|
+          @summarization.happens(memo, tuple)
+        end
+        yield @summarization.finalize(agg)
+      end
+
+    end # class Aggregate
+  end # Engine
+end # module Alf
---
+++ spec/unit/alf-engine/test_aggregate.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+module Alf
+  module Engine
+    describe Aggregate do
+
+      it 'should work on an empty operand' do
+        Autonum.new([], Summarization[{}]).to_a.should eq([])
+      end
+
+      it 'should work on a non empty operand' do
+        rel = [
+          {:name => "Jones", :price => 12.0, :id => 1},
+          {:name => "Smith", :price => 10.0, :id => 2}
+        ]
+        agg = Summarization[:size  => "count", 
+                            :total => "sum{ price }"]
+        exp = [
+          {:size => 2, :total => 22.0}
+        ]
+        Aggregate.new(rel, agg).to_a.should eq(exp)
+      end
+
+    end
+  end # module Engine
+end # module Alf    
+
