0f8d7bb67ee70eebfc01bd951013dda7348395ec
Bernard Lambeau <blambeau@gmail.com>
2011-12-07 17:50:14 +0100

    Union operator uses Concat + Compact

--- lib/alf/operator/relational/union.rb
+++ lib/alf/operator/relational/union.rb
@@ -1,32 +1,17 @@
 module Alf
   module Operator::Relational
     class Union < Alf::Operator()
-      include Operator::Relational, Operator::Shortcut, Operator::Binary
-      
+      include Relational, Binary
+
       signature do |s|
       end
-      
-      class DisjointBased
-        include Operator, Operator::Binary
-      
-        protected
-        
-        def _each
-          left.each(&Proc.new)
-          right.each(&Proc.new)
-        end
-        
+
+      def each(&block)
+        op = Engine::Concat.new([left, right])
+        op = Engine::Compact.new(op)
+        op.each(&block)
       end
-      
-      protected
-      
-      # (see Shortcut#longexpr)
-      def longexpr
-        chain Operator::NonRelational::Compact.new,
-              DisjointBased.new,
-              datasets 
-      end
-      
+
     end # class Union
   end # module Operator::Relational
 end # module Alf
