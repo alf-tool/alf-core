a99e2fc810495725d7c7e28792c142adc74cde4c
Bernard Lambeau <blambeau@gmail.com>
2011-12-07 09:18:28 +0100

    Iterator in its own file

--- lib/alf.rb
+++ lib/alf.rb
@@ -16,30 +16,7 @@ module Alf
   require 'alf/types'
   require 'alf/tools'
   require 'alf/environment'
-
-  #
-  # Marker module for all elements implementing tuple iterators.
-  #
-  # At first glance, an iterator is nothing else than an Enumerable that serves 
-  # tuples (represented by ruby hashes). However, this module helps Alf's internal
-  # classes to recognize enumerables that may safely be considered as tuple 
-  # iterators from other enumerables. For this reason, all elements that would
-  # like to participate to an iteration chain (that is, an logical operator 
-  # implementation) should be marked with this module. This is the case for 
-  # all Readers and Operators defined in Alf.
-  #
-  # Moreover, an Iterator should always define a {#pipe} method, which is the
-  # natural way to define the input and execution environment of operators and 
-  # readers. 
-  # 
-  module Iterator
-    include Enumerable
-
-    require 'alf/iterator/class_methods'
-    require 'alf/iterator/base'
-    require 'alf/iterator/proxy'
-  end # module Iterator
-
+  require 'alf/iterator'
   require 'alf/reader'
   require 'alf/renderer'
 
