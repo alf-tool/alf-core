3002c46b3f2143b2cf0f7fc1bd50ad4c2dda932e
Bernard Lambeau <blambeau@gmail.com>
2011-12-08 17:37:17 +0100

    Intersect is a particular case of join

--- lib/alf/operator/relational/intersect.rb
+++ lib/alf/operator/relational/intersect.rb
@@ -1,37 +1,16 @@
 module Alf
   module Operator::Relational
     class Intersect < Alf::Operator()
-      include Operator, Operator::Relational, Operator::Shortcut, Operator::Binary
-      
+      include Relational, Binary
+
       signature do |s|
       end
-      
-      class HashBased
-        include Operator, Operator::Binary
-      
-        protected
-        
-        def _prepare
-          @index = Hash.new
-          right.each{|t| @index[t] = true}
-        end
-        
-        def _each
-          left.each do |left_tuple|
-            yield(left_tuple) if @index.has_key?(left_tuple)
-          end
-        end
-        
+
+      # (see Operator#each)
+      def each(&block)
+        Engine::Join::Hash.new(left, right).each(&block)
       end
-      
-      protected
-      
-      # (see Shortcut#longexpr)
-      def longexpr
-        chain HashBased.new,
-              datasets 
-      end
-      
+
     end # class Intersect
   end # module Operator::Relational
 end # module Alf
