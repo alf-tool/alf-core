7edc9fbf2f0a79deb789910b2afd1a3432201395
Bernard Lambeau <blambeau@gmail.com>
2011-12-08 18:31:04 +0100

    Minus uses Engine::Semi

--- lib/alf/operator/relational/minus.rb
+++ lib/alf/operator/relational/minus.rb
@@ -1,37 +1,15 @@
 module Alf
   module Operator::Relational
     class Minus < Alf::Operator()
-      include Operator::Relational, Operator::Shortcut, Operator::Binary
-      
+      include Relational, Binary
+
       signature do |s|
       end
-      
-      class HashBased
-        include Operator, Operator::Binary
-      
-        protected
-        
-        def _prepare
-          @index = Hash.new
-          right.each{|t| @index[t] = true}
-        end
-        
-        def _each
-          left.each do |left_tuple|
-            yield(left_tuple) unless @index.has_key?(left_tuple)
-          end
-        end
-        
+
+      def each(&block)
+        Engine::Semi::Hash.new(left, right, false).each(&block)
       end
-      
-      protected
-      
-      # (see Shortcut#longexpr)
-      def longexpr
-        chain HashBased.new,
-              datasets 
-      end
-      
+
     end # class Minus
   end # module Operator::Relational
 end # module Alf
