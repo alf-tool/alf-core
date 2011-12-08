0760c066f815fadc1ea53629690f0c2249c59fdd
Bernard Lambeau <blambeau@gmail.com>
2011-12-08 18:29:56 +0100

    Matching and NotMatching use Engine::Semi

--- lib/alf/operator/relational/matching.rb
+++ lib/alf/operator/relational/matching.rb
@@ -1,45 +1,15 @@
 module Alf
   module Operator::Relational
     class Matching < Alf::Operator()
-      include Operator::Relational, Operator::Shortcut, Operator::Binary
-      
+      include Relational, Binary
+
       signature do |s|
       end
-      
-      #
-      # Performs a Matching of two relations through a Hash buffer on the right
-      # one.
-      #
-      class HashBased
-        include Operator, Operator::Binary
-      
-        # (see Operator#_each)
-        def _each
-          seen, key = nil, nil
-          left.each do |left_tuple|
-            seen ||= begin
-              h = Hash.new
-              right.each do |right_tuple|
-                key ||= coerce(left_tuple.keys & right_tuple.keys, AttrList)
-                h[key.project_tuple(right_tuple)] = true
-              end
-              key ||= coerce([], AttrList)
-              h
-            end
-            yield(left_tuple) if seen.has_key?(key.project_tuple(left_tuple))
-          end
-        end
-        
-      end # class HashBased
-      
-      protected
-      
-      # (see Shortcut#longexpr)
-      def longexpr
-        chain HashBased.new,
-              datasets 
+
+      def each(&block)
+        Engine::Semi::Hash.new(left, right, true).each(&block)
       end
-      
+
     end # class Matching
   end # module Operator::Relational
 end # module Alf
