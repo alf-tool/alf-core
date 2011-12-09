6d3981ef5d39b18ab27f07026af4a799c213bb2a
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:39:35 +0100

    Refactor Restrict test (no-pipe)

--- spec/unit/alf-core/operator/relational/test_restrict.rb
+++ spec/unit/alf-core/operator/relational/test_restrict.rb
@@ -2,55 +2,52 @@ require 'spec_helper'
 module Alf
   module Operator::Relational
     describe Restrict do
-        
+
       let(:operator_class){ Restrict }
       it_should_behave_like("An operator class")
-        
+
       let(:input) {[
         {:tested => 1, :other => "b"},
         {:tested => 30, :other => "a"},
       ]}
-  
+
       let(:expected){[
         {:tested => 1, :other => "b"}
       ]}
-  
+
       subject{ operator.to_a }
-  
-      describe "when used with no argument" do
-        let(:operator){ Restrict.run(%w{}) }
-        before{ operator.pipe(input) }
-        it { should == input }
+
+      context "with no argument" do
+        let(:operator){ Restrict.run([input]) }
+        it { should eq(input) }
       end
-  
-      describe "when used with a string" do
-        describe "when factored with commandline args" do
-          let(:operator){ Restrict.run(["--", "tested < 10"]) }
-          before{ operator.pipe(input) }
-          it { should == expected }
+
+      context "with a string" do
+        context "with .run" do
+          let(:operator){ Restrict.run([input, "--", "tested < 10"]) }
+          it { should eq(expected) }
         end
-        describe "when factored with Lispy" do
+        context "with Lispy" do
           let(:operator){ Lispy.restrict(input, "tested < 10") }
-          it { should == expected }
+          it { should eq(expected) }
         end
       end
-  
-      describe "when used with arguments" do
-        describe "when factored with commandline args" do
-          let(:operator){ Restrict.run(["--", "tested", "1"]) }
-          before{ operator.pipe(input) }
-          it { should == expected }
+
+      context "with arguments" do
+        context "with .run" do
+          let(:operator){ Restrict.run([input, "--", "tested", "1"]) }
+          it { should eq(expected) }
         end
-        describe "when factored with Lispy and Proc" do
+        context "with Lispy and Proc" do
           let(:operator){ Lispy.restrict(input, lambda{ tested < 10 }) }
-          it { should == expected }
+          it { should eq(expected) }
         end
-        describe "when factored with Lispy and array" do
+        context "with Lispy and array" do
           let(:operator){ Lispy.restrict(input, [:tested, 1]) }
-          it { should == expected }
+          it { should eq(expected) }
         end
       end
-  
+
     end 
   end
 end
