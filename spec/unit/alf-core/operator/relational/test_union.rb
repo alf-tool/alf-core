63052b64e34bf3829a079ddd22457680c6db0153
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:43:27 +0100

    Refactor Union test (no-pipe)

--- spec/unit/alf-core/operator/relational/test_union.rb
+++ spec/unit/alf-core/operator/relational/test_union.rb
@@ -2,34 +2,34 @@ require 'spec_helper'
 module Alf
   module Operator::Relational
     describe Union do
-        
+
       let(:operator_class){ Union }
       it_should_behave_like("An operator class")
-        
+
       let(:left) {[
         {:city => 'London'},
         {:city => 'Paris'},
         {:city => 'Paris'}
       ]}
-  
+
       let(:right) {[
         {:city => 'Oslo'},
         {:city => 'Paris'}
       ]}
-  
-      let(:operator){ Union.run([]) }
+
+      let(:operator){ Union.run(args) }
       subject{ operator.to_a }
-  
-      describe "when applied on non disjoint sets" do
-        before{ operator.pipe [left, right] }
+
+      context "when applied on non disjoint sets" do
+        let(:args){ [left, right] }
         let(:expected){[
           {:city => 'London'},
           {:city => 'Paris'},
           {:city => 'Oslo'}
         ]}
-        it { should == expected }
+        it { should eq(expected) }
       end
-  
+
     end 
   end
 end
