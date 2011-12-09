daaf182a8d770e490ce0eaa7c1cc6502fc6aa8b0
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:26:24 +0100

    Refactor Minus test (no-pipe)

--- spec/unit/alf-core/operator/relational/test_minus.rb
+++ spec/unit/alf-core/operator/relational/test_minus.rb
@@ -2,55 +2,55 @@ require 'spec_helper'
 module Alf
   module Operator::Relational
     describe Minus do
-        
+
       let(:operator_class){ Minus }
       it_should_behave_like("An operator class")
-        
+
       let(:left) {[
         {:sid => 'S1', :city => 'London'},
         {:sid => 'S2', :city => 'Paris'},
         {:sid => 'S3', :city => 'Paris'}
       ]}
-        
+
       let(:right) {[
         {:sid => 'S2', :city => 'Paris'},
         {:sid => 'S1', :city => 'London'},
       ]}
-        
+
       let(:disjoint) {[
         {:sid => 'S4', :city => 'Oslo'},
         {:sid => 'S5', :city => 'Bruxelles'},
       ]}
-  
-      let(:operator){ Minus.run([]) }
+
+      let(:operator){ Minus.run(args) }
       subject{ operator.to_a }
-  
-      describe "when applied on the same operand twice" do
-        before{ operator.pipe [left, left] }
+
+      context "when applied on the same operand twice" do
+        let(:args){ [left, left] }
         it { should be_empty }
       end
-      
-      describe "when applied on operands sharing tuples" do
-        before{ operator.pipe [left, right] }
+
+      context "when applied on operands sharing tuples" do
+        let(:args){ [left, right] }
         let(:expected) {[
           {:sid => 'S3', :city => 'Paris'}
         ]}
-        it { should == expected }
+        it { should eq(expected) }
       end
-    
-      describe "when applied on disjoint operands" do
-        before{ operator.pipe [left, disjoint] }
-        it { should == left }
+
+      context "when applied on disjoint operands" do
+        let(:args){ [left, disjoint] }
+        it { should eq(left) }
       end
-      
-      describe "when factored with Lispy" do
+
+      context "when factored with Lispy" do
         let(:operator){ Lispy.minus(left, right) }
         let(:expected) {[
           {:sid => 'S3', :city => 'Paris'}
         ]}
-        it { should == expected }
+        it { should eq(expected) }
       end
-  
-    end 
+
+    end
   end
 end
