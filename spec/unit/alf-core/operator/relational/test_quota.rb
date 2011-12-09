9b56287e3476c4b54ecd0ee082ae22d63f7fb3e1
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:30:04 +0100

    Refactor Quota test (no-pipe)

--- spec/unit/alf-core/operator/relational/test_quota.rb
+++ spec/unit/alf-core/operator/relational/test_quota.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 module Alf
   module Operator::Relational
     describe Quota do
-        
+
       let(:operator_class){ Quota }
       it_should_behave_like("An operator class")
-        
+
       let(:input) {[
         {:a => "via_method", :time => 1},
         {:a => "via_method", :time => 2},
@@ -13,7 +13,7 @@ module Alf
         {:a => "via_reader", :time => 4},
         {:a => "via_reader", :time => 2},
       ]}
-  
+
       let(:expected) {[
         {:a => "via_method", :time => 1, :time_sum => 1, :time_max => 1},
         {:a => "via_method", :time => 2, :time_sum => 3, :time_max => 2},
@@ -21,23 +21,22 @@ module Alf
         {:a => "via_reader", :time => 2, :time_sum => 2, :time_max => 2},
         {:a => "via_reader", :time => 4, :time_sum => 6, :time_max => 4},
       ]}
-  
+
       subject{ operator.to_a }
-  
-      describe "When factored with commandline args" do
+
+      context "with .run" do
         let(:aggs){ ["--", "a", "--", "time", "--", "time_sum", "sum{ time }", "time_max", "max{ time }"] }
-        let(:operator){ Quota.run(aggs) }
-        before{ operator.pipe(input) }
+        let(:operator){ Quota.run([input] + aggs) }
         it { should == expected }
       end
-  
-      describe "When factored with Lispy" do
+
+      context "with Lispy" do
         let(:aggs){{:time_sum => Aggregator.sum{ time },
                     :time_max => Aggregator.max{ time }}} 
         let(:operator){ Lispy.quota(input, [:a], [:time], aggs) }
         it { should == expected }
       end
-  
-    end 
+
+    end
   end
 end
