314b3f59b0f22d4b9c956b4f50e71371f4967dba
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:23:28 +0100

    Refactor Intersect test (no-pipe)

--- spec/unit/alf-core/operator/relational/test_intersect.rb
+++ spec/unit/alf-core/operator/relational/test_intersect.rb
@@ -2,49 +2,49 @@ require 'spec_helper'
 module Alf
   module Operator::Relational
     describe Intersect do
-        
+
       let(:operator_class){ Intersect }
       it_should_behave_like("An operator class")
-        
+
       let(:left) {[
         {:sid => 'S1', :city => 'London'},
         {:sid => 'S2', :city => 'Paris'},
         {:sid => 'S3', :city => 'Paris'}
       ]}
-        
+
       let(:right) {[
         {:sid => 'S2', :city => 'Paris'},
         {:sid => 'S1', :city => 'London'},
       ]}
-        
+
       let(:disjoint) {[
         {:sid => 'S4', :city => 'Oslo'},
         {:sid => 'S5', :city => 'Bruxelles'},
       ]}
-  
-      let(:operator){ Intersect.run([]) }
+
+      let(:operator){ Intersect.run(args) }
       subject{ operator.to_a }
-  
-      describe "when applied on the same operand twice" do
-        before{ operator.pipe [left, left] }
-        it { should == left }
+
+      context "with same operand twice" do
+        let(:args){ [left, left] }
+        it { should eq(left) }
       end
-      
-      describe "when applied on operands sharing tuples" do
-        before{ operator.pipe [left, right] }
+
+      context "on operands sharing tuples" do
+        let(:args){ [left, right] }
         let(:expected) {[
           {:sid => 'S1', :city => 'London'},
           {:sid => 'S2', :city => 'Paris'},
         ]}
-        it { should == expected }
+        it { should eq(expected) }
       end
-    
-      describe "when applied on disjoint operands" do
-        before{ operator.pipe [left, disjoint] }
+
+      context "on disjoint operands" do
+        let(:args){ [left, disjoint] }
         it { should be_empty }
       end
-      
-      describe "when factored with Lispy" do
+
+      context "with Lispy" do
         let(:operator){ Lispy.intersect(left, right) }
         let(:expected) {[
           {:sid => 'S1', :city => 'London'},
@@ -52,7 +52,7 @@ module Alf
         ]}
         it { should == expected }
       end
-  
-    end 
+
+    end
   end
 end
