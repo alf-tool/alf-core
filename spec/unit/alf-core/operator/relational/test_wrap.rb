277cc7dd725a3527095f88c89fed2a059f37bc19
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:45:16 +0100

    Refactor Wrap test (no-pipe)

--- spec/unit/alf-core/operator/relational/test_wrap.rb
+++ spec/unit/alf-core/operator/relational/test_wrap.rb
@@ -2,31 +2,30 @@ require 'spec_helper'
 module Alf
   module Operator::Relational
     describe Wrap do
-        
+
       let(:operator_class){ Wrap }
       it_should_behave_like("An operator class")
-        
+
       let(:input) {[
         {:a => "a", :b => "b", :c => "c"},
       ]}
-  
+
       let(:expected) {[
         {:wraped => {:a => "a", :b => "b"}, :c => "c"}
       ]}
-  
+
       subject{ operator.to_a }
-  
-      describe "when factored with commandline args" do
-        let(:operator){ Wrap.run(["--", "a", "b", "--", "wraped"]) }
-        before{ operator.pipe(input) }
-        it { should == expected }
+
+      context "with .run" do
+        let(:operator){ Wrap.run([input, "--", "a", "b", "--", "wraped"]) }
+        it { should eq(expected) }
       end
-  
-      describe "when factored with Lispy" do
+
+      context "with Lispy" do
         let(:operator){ Lispy.wrap(input, [:a, :b], :wraped) }
-        it { should == expected }
+        it { should eq(expected) }
       end
-  
-    end 
+
+    end
   end
 end
