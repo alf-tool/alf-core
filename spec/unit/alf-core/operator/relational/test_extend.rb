b8a14caf107c3867be1965b7f707b9805245940b
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:17:18 +0100

    Refactor Extend test (no-pipe)

--- spec/unit/alf-core/operator/relational/test_extend.rb
+++ spec/unit/alf-core/operator/relational/test_extend.rb
@@ -2,33 +2,32 @@ require 'spec_helper'
 module Alf
   module Operator::Relational
     describe Extend do
-        
+
       let(:operator_class){ Extend }
       it_should_behave_like("An operator class")
-        
+
       let(:input) {[
         {:tested => 1,  :other => "b"},
         {:tested => 30, :other => "a"},
       ]}
-  
+
       let(:expected){[
         {:tested => 1,  :other => "b", :big => false},
         {:tested => 30, :other => "a", :big => true},
       ]}
-  
+
       subject{ operator.to_a }
-  
-      describe "When factored with Lispy" do 
+
+      context "with Lispy" do 
         let(:operator){ Lispy.extend(input, :big => lambda{ tested > 10 }) }
         it{ should == expected }
       end
-  
-      describe "When factored from commandline args" do
-        let(:operator){ Extend.run(%w{-- big} + ["tested > 10"]) }
-        before{ operator.pipe(input) }
+
+      context "with .run" do
+        let(:operator){ Extend.run([input] + %w{-- big} + ["tested > 10"]) }
         it{ should == expected }
       end
-  
+
     end 
   end
 end
