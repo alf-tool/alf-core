dbd9161faf0dbe7933fee0154cbf0d74a10bd231
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:44:20 +0100

    Refactor Unwrap test (no-pipe)

--- spec/unit/alf-core/operator/relational/test_unwrap.rb
+++ spec/unit/alf-core/operator/relational/test_unwrap.rb
@@ -2,31 +2,30 @@ require 'spec_helper'
 module Alf
   module Operator::Relational
     describe Unwrap do
-        
+
       let(:operator_class){ Unwrap }
       it_should_behave_like("An operator class")
-        
+
       let(:input) {[
         {:wrapped => {:a => "a", :b => "b"}, :c => "c"}
       ]}
-  
+
       let(:expected) {[
         {:a => "a", :b => "b", :c => "c"},
       ]}
-  
+
       subject{ operator.to_a }
-  
-      describe "when factored with commandline args" do
-        let(:operator){ Unwrap.run(%w{-- wrapped}) }
-        before{ operator.pipe(input) }
-        it { should == expected }
+
+      context "with .run" do
+        let(:operator){ Unwrap.run([input] + %w{-- wrapped}) }
+        it { should eq(expected) }
       end
-  
-      describe "when factored with Lispy" do
+
+      context "when factored with Lispy" do
         let(:operator){ Lispy.unwrap(input, :wrapped) }
-        it { should == expected }
+        it { should eq(expected) }
       end
-  
-    end 
+
+    end
   end
 end
