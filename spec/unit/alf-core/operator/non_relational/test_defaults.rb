37fe23adcfa24fafb17cf785c33f5994024b9845
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:12:34 +0100

    Refactor Defaults test (no-pipe)

--- spec/unit/alf-core/operator/non_relational/test_defaults.rb
+++ spec/unit/alf-core/operator/non_relational/test_defaults.rb
@@ -2,55 +2,48 @@ require 'spec_helper'
 module Alf
   module Operator::NonRelational
     describe Defaults do
-      
+
       let(:operator_class){ Defaults }
       it_should_behave_like("An operator class")
-        
+
       subject{ operator.to_a }
-  
-      describe "when used without --strict" do
-        
+
+      context "--no-strict" do
         let(:input) {[
           {:a => nil, :b => "b"},
         ]}
-    
         let(:expected) {[
           {:a => 1, :b => "b", :c => "blue"},
         ]}
-    
-        describe "When factored with Lispy" do 
+
+        context "with Lispy" do 
           let(:operator){ Lispy.defaults(input, :a => 1, :c => "blue") }
           it{ should == expected }
         end
-    
-        describe "When factored from commandline args" do
-          let(:operator){ Defaults.run(%w{-- a 1 c 'blue'}) }
-          before{ operator.pipe(input) }
+
+        context "with .run" do
+          let(:operator){ Defaults.run([input] +%w{-- a 1 c 'blue'}) }
           it{ should == expected }
         end
-  
-      end
-        
-      describe "when used with --strict" do
-        
+      end # --no-strict
+
+      describe "--strict" do
         let(:input) {[
           {:a => 3,   :b => "b", :c => "blue"},
           {:a => nil, :b => "b", :c => "blue"},
         ]}
-    
         let(:expected) {[
           {:a => 3, :b => "b"},
           {:a => 1, :b => "b"},
         ]}
-    
-        describe "When factored with Lispy" do 
+
+        context "with Lispy" do 
           let(:operator){ Lispy.defaults(input, {:a => 1, :b => "b"}, :strict => true) }
           it{ should == expected }
         end
-    
-      end
-      
-      describe "when used with tuple expressions" do
+      end # --strict
+
+      context "with tuple expressions" do
         let(:input) {[
           {:a => nil, :b => "b"},
         ]}
@@ -61,8 +54,8 @@ module Alf
           Lispy.defaults(input, {:a => TupleExpression.coerce("b")}) 
         }
         it{ should == expected }
-      end
-        
-    end 
+      end # tuple expressions
+
+    end
   end
 end
