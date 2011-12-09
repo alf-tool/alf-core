f00f8a720e484a1a1abf7d858a6adc941fdc9f9d
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:42:23 +0100

    Refactor Ungroup test (no-pipe)

--- spec/unit/alf-core/operator/relational/test_ungroup.rb
+++ spec/unit/alf-core/operator/relational/test_ungroup.rb
@@ -2,34 +2,33 @@ require 'spec_helper'
 module Alf
   module Operator::Relational
     describe Ungroup do
-        
+
       let(:operator_class){ Ungroup }
       it_should_behave_like("An operator class")
-        
+
       let(:input) {[
         {:a => "via_method", :as => [{:time => 1, :b => "b"}, {:time => 2, :b => "b"}]},
         {:a => "via_reader", :as => [{:time => 3, :b => "b"}]},
       ]}
-  
+
       let(:expected) {[
         {:a => "via_method", :time => 1, :b => "b"},
         {:a => "via_method", :time => 2, :b => "b"},
         {:a => "via_reader", :time => 3, :b => "b"},
       ]}
-  
+
       subject{ operator.to_a.sort{|k1,k2| k1[:time] <=> k2[:time]} } 
-  
-      describe "when factored with commandline args" do
-        let(:operator) { Ungroup.run(%w{-- as}) } 
-        before{ operator.pipe(input) }
+
+      context "with .run" do
+        let(:operator) { Ungroup.run([input] + %w{-- as}) } 
         it { should == expected }
       end
-  
-      describe "when factored with Lispy" do
+
+      context "with Lispy" do
         let(:operator) { Lispy.ungroup(input, :as) } 
         it { should == expected }
       end
-  
-    end 
+
+    end
   end
 end
