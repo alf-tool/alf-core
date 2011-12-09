e967ae818d8f87873482626f7eebd92ec3fa692e
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:06:08 +0100

    Refactor Coerce test (no-pipe)

--- spec/unit/alf-core/operator/non_relational/test_coerce.rb
+++ spec/unit/alf-core/operator/non_relational/test_coerce.rb
@@ -2,34 +2,32 @@ require 'spec_helper'
 module Alf
   module Operator::NonRelational
     describe Coerce do
-  
+
       let(:operator_class){ Coerce }
       it_should_behave_like("An operator class")
-        
+
       let(:input) {Alf::Relation[
         {:a => "12", :b => "14.0"},
       ]}
-  
+
       subject{ operator.to_rel }
-  
-      describe "When used without --strict" do
+
+      context "--no-strict" do
         let(:expected){Alf::Relation[
           {:a => 12, :b => 14.0}
         ]}
-        
-        describe "when factored from commandline" do
-          let(:operator){ Coerce.run(%w{-- a Integer b Float}) }
-          before{ operator.pipe(input) }
-          it { should == expected } 
-        end
-  
-        describe "when factored with Lispy" do
+
+        context "with Lispy" do
           let(:operator){ Lispy.coerce(input, :a => Integer, :b => Float) }
+          it { should == expected }
+        end
+
+        context "from commandline" do
+          let(:operator){ Coerce.run([input] + %w{-- a Integer b Float}) }
           it { should == expected } 
         end
-  
-      end
-  
+      end # --no-strict
+
     end 
   end
 end
