cf7a3e4020fba8e370bf9b8294fe769e06ad9c24
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:41:35 +0100

    Refactor Summarize test (no-pipe)

--- spec/unit/alf-core/operator/relational/test_summarize.rb
+++ spec/unit/alf-core/operator/relational/test_summarize.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 module Alf
   module Operator::Relational
     describe Summarize do
-        
+
       let(:operator_class){ Summarize }
       it_should_behave_like("An operator class")
-        
+
       let(:input) {[
         {:a => "via_reader", :time => 2},
         {:a => "via_method", :time => 1},
@@ -13,50 +13,44 @@ module Alf
         {:a => "via_reader", :time => 4},
         {:a => "via_method", :time => 1},
       ]}
-  
+
       let(:expected) {[
         {:a => "via_method", :time_sum => 4, :time_max => 2, :time_avg => 4.0/3},
         {:a => "via_reader", :time_sum => 6, :time_max => 4, :time_avg => 6.0/2},
       ]}
-  
+
       let(:aggs){{:time_sum => Aggregator.sum{ time },
                   :time_max => Aggregator.max{ time },
                   :time_avg => Aggregator.avg{ time }}} 
 
       subject{ operator.to_a.sort{|t1,t2| t1[:a] <=> t2[:a]} }
-  
-      describe "without allbut" do
-        
-        describe "When factored with commandline args" do
+
+      context "--no-allbut" do
+        context "with .run" do
           let(:aggs){ ["--", "a", "--", "time_sum", "sum{ time }", "time_max", "max{ time }", "time_avg", "avg{ time }"] }
-          let(:operator){ Summarize.run(aggs) }
-          before{ operator.pipe(input) }
-          it { should == expected }
+          let(:operator){ Summarize.run([input] + aggs) }
+          it { should eq(expected) }
         end
-    
-        describe "When factored with Lispy" do
+
+        context "with Lispy" do
           let(:operator){ Lispy.summarize(input, [:a], aggs) }
-          it { should == expected }
+          it { should eq(expected) }
         end
+      end # --no-allbut
 
-      end
-  
-      describe "with allbut" do
-        
-        describe "When factored with commandline args" do
+      context "--allbut" do
+        context "with .run" do
           let(:aggs){ ["--allbut", "--", "time", "--", "time_sum", "sum{ time }", "time_max", "max{ time }", "time_avg", "avg{ time }"] }
-          let(:operator){ Summarize.run(aggs) }
-          before{ operator.pipe(input) }
-          it { should == expected }
-        end
-    
-        describe "When factored with Lispy" do
-          let(:operator){ Lispy.summarize(input, [:time], aggs, :allbut => true) }
-          it { should == expected }
+          let(:operator){ Summarize.run([input] + aggs) }
+          it { should eq(expected) }
         end
 
-      end
-  
+        context "with Lispy" do
+          let(:operator){ Lispy.summarize(input, [:time], aggs, :allbut => true) }
+          it { should eq(expected) }
+        end
+      end # --allbut
+
     end 
   end
 end
