811f776834f0b584e31b5edc0c888bcda01202b0
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:10:29 +0100

    Refactor Compact test (no-pipe)

--- spec/unit/alf-core/operator/non_relational/test_compact.rb
+++ spec/unit/alf-core/operator/non_relational/test_compact.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 module Alf
   module Operator::NonRelational
     describe Compact do
-        
+
       let(:operator_class){ Compact }
       it_should_behave_like("An operator class")
-        
+
       let(:input) {[
         {:a => "via_method", :time => 1, :b => "b"},
         {:a => "via_reader", :time => 3, :b => "b"},
@@ -13,26 +13,25 @@ module Alf
         {:a => "via_reader", :time => 3, :b => "b"},
         {:a => "via_method", :time => 1, :b => "b"},
       ]}
-  
+
       let(:expected) {[
         {:a => "via_method", :time => 1, :b => "b"},
         {:a => "via_reader", :time => 3, :b => "b"},
         {:a => "via_method", :time => 2, :b => "b"},
       ]}
-  
+
       subject{ operator.to_a }
-  
-      describe "when factored with commandline args" do
-        let(:operator){ Compact.run(%w{}) }
-        before{ operator.pipe(input) }
-        it { should == expected }
-      end
-  
-      describe "when factored with Lispy" do
+
+      context "with Lispy" do
         let(:operator){ Lispy.compact(input) }
         it { should == expected }
       end
-  
-    end 
+
+      context "with .run" do
+        let(:operator){ Compact.run([input]) }
+        it { should == expected }
+      end
+
+    end
   end
 end
