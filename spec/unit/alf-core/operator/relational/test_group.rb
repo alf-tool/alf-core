a6566ca4e7bbc7dde0e23dfe1b29be359b8d23be
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 15:18:57 +0100

    Refactor Group test (no-pipe)

--- spec/unit/alf-core/operator/relational/test_group.rb
+++ spec/unit/alf-core/operator/relational/test_group.rb
@@ -2,54 +2,48 @@ require 'spec_helper'
 module Alf
   module Operator::Relational
     describe Group do
-        
+
       let(:operator_class){ Group }
       it_should_behave_like("An operator class")
-        
+
       let(:input) {[
         {:a => "via_method", :time => 1, :b => "b"},
         {:a => "via_method", :time => 2, :b => "b"},
         {:a => "via_reader", :time => 3, :b => "b"},
       ]}
-  
+
       let(:expected) {[
         {:a => "via_method", :as => Alf::Relation[{:time => 1, :b => "b"}, 
                                                   {:time => 2, :b => "b"}]},
         {:a => "via_reader", :as => Alf::Relation[{:time => 3, :b => "b"}]},
       ]}
-  
+
       subject{ operator.to_a.sort{|k1,k2| k1[:a] <=> k2[:a]} }
-  
-      describe "without --allbut" do
-  
-        describe "when factored with commandline args" do
-          let(:operator){ Group.run(["--", "time", "b", "--", "as"]) }
-          before{ operator.pipe(input) }
+
+      context "--no-allbut" do
+        context "with .run" do
+          let(:operator){ Group.run([input, "--", "time", "b", "--", "as"]) }
           it { should == expected }
         end
-    
-        describe "when factored with Lispy" do
+
+        context "with Lispy" do
           let(:operator){ Lispy.group(input, [:time, :b], :as) }
           it { should == expected }
         end
-        
-      end
+      end # --no-allbut
   
-      describe "with --allbut" do
-          
-        describe "when factored with commandline args" do
-          let(:operator){ Group.run(["--allbut", "--","a", "--", "as"]) }
-          before{ operator.pipe(input) }
+      context "--allbut" do
+        context "with .run" do
+          let(:operator){ Group.run([input, "--allbut", "--","a", "--", "as"]) }
           it { should == expected }
         end
-    
-        describe "when factored with Lispy" do
+
+        context "with Lispy" do
           let(:operator){ Lispy.group(input, [:a], :as, :allbut => true) }
           it { should == expected }
         end
-        
-      end
-  
+      end # --allbut
+
     end 
   end
 end
