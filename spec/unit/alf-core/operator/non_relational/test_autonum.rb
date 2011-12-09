c55b99ec8e9e15c4b5de525b9362521968484496
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 11:30:36 +0100

    Clean the Autonum test

--- spec/unit/alf-core/operator/non_relational/test_autonum.rb
+++ spec/unit/alf-core/operator/non_relational/test_autonum.rb
@@ -2,60 +2,60 @@ require 'spec_helper'
 module Alf
   module Operator::NonRelational
     describe Autonum do
-        
+
       let(:operator_class){ Autonum }
       it_should_behave_like("An operator class")
-        
+
       let(:input) {[
         {:a => "a"},
         {:a => "b"},
         {:a => "a"},
       ]}
-  
+
       subject{ operator.to_a }
-  
-      describe "without providing an attribute name" do
-        
+
+      context "with default attribute name" do
+
         let(:expected){[
           {:a => "a", :autonum => 0},
           {:a => "b", :autonum => 1},
           {:a => "a", :autonum => 2},
         ]}
-    
+
         describe "When factored with Lispy" do 
           let(:operator){ Lispy.autonum(input) }
           it{ should == expected }
         end
-    
+
         describe "When factored from commandline args" do
           let(:operator){ Autonum.run([]) }
           before{ operator.pipe(input) }
           it{ should == expected }
         end
-  
+
       end
-      
-      describe "when providing an attribute name" do
-        
+
+      context "when providing an attribute name" do
+
         let(:expected){[
           {:a => "a", :unique => 0},
           {:a => "b", :unique => 1},
           {:a => "a", :unique => 2},
         ]}
-    
+
         describe "When factored with Lispy" do 
           let(:operator){ Lispy.autonum(input, :unique) }
           it{ should == expected }
         end
-    
+
         describe "When factored from commandline args" do
           let(:operator){ Autonum.run(["--", "unique"]) }
           before{ operator.pipe(input) }
           it{ should == expected }
         end
-  
+
       end
-      
-    end 
+
+    end
   end
 end
