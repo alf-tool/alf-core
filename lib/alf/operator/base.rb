63e93caa1f2a065f4fbf4837bca88d2cbc75dbeb
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 09:47:59 +0100

    Clean the Operator contract (1/x)

--- lib/alf/operator/base.rb
+++ lib/alf/operator/base.rb
@@ -5,68 +5,35 @@ module Alf
     #
     module Base
 
-      #
       # Input datasets
-      #
       attr_accessor :datasets
-      
-      #
-      # Optional environment
-      #
+
+      # @return [Environment] Environment to use (optional)
       attr_accessor :environment
-      
-      #
+
       # Create an operator instance
-      #
       def initialize(*args)
         signature.parse_args(args, self)
       end
-  
+
       # 
       # Sets the operator input
       #
       def pipe(input, env = environment)
         raise NotImplementedError, "Operator#pipe should be overriden"
       end
-      
-      #
-      # Returns operator signature.
-      #
+
+      # @return [Signature] the operator signature.
       def signature
         self.class.signature
       end
 
-      #
       # Yields each tuple in turn 
       #
-      # This method is implemented in a way that ensures that all operators are 
-      # thread safe. It is not intended to be overriden, use _each instead.
-      # 
       def each
-        op = self.dup
-        op._prepare
-        op._each(&Proc.new)
       end
-      
-      protected
-  
-      #
-      # Prepares the iterator before subsequent call to _each.
-      #
-      # This method is intended to be overriden by suclasses to install what's 
-      # need for successful iteration. The default implementation does nothing.
-      #
-      def _prepare 
-      end
-  
-      # Internal implementation of the iterator.
-      #
-      # This method must be implemented by subclasses. It is safe to use instance
-      # variables (typically initialized in _prepare) here.
-      # 
-      def _each
-      end
-      
+      undef :each
+
       private :datasets=, :environment=
     end # module Base
     include Base
