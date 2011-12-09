40159b7a8b109750aa03eed2d5dea1f668e744c3
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 09:04:25 +0100

    Summarize operation uses the Engine

--- lib/alf/operator/relational/summarize.rb
+++ lib/alf/operator/relational/summarize.rb
@@ -1,80 +1,23 @@
 module Alf
   module Operator::Relational
     class Summarize < Alf::Operator()
-      include Operator::Relational, Operator::Shortcut, Operator::Unary
-      
+      include Relational, Unary
+
       signature do |s|
         s.argument :by,            AttrList, []
         s.argument :summarization, Summarization, {}
         s.option   :allbut,        Boolean, false, "Summarize on all but specified attributes?"
       end
-      
-      # Summarizes according to a complete order
-      class SortBased
-        include Operator, Operator::Cesure
-  
-        def initialize(by_key, allbut, summarization)
-          @by_key, @allbut, @summarization = by_key, allbut, summarization
-        end
-  
-        protected 
-  
-        # (see Operator::Cesure#project)
-        def project(tuple)
-          @by_key.project_tuple(tuple, @allbut)
-        end
-        
-        # (see Operator::Cesure#start_cesure)
-        def start_cesure(key, receiver)
-          @aggs = @summarization.least
-        end
-  
-        # (see Operator::Cesure#accumulate_cesure)
-        def accumulate_cesure(tuple, receiver)
-          @aggs = @summarization.happens(@aggs, tuple)
-        end
-  
-        # (see Operator::Cesure#flush_cesure)
-        def flush_cesure(key, receiver)
-          @aggs = @summarization.finalize(@aggs)
-          receiver.call key.merge(@aggs)
-        end
-  
-      end # class SortBased
 
-      # Summarizes in-memory with a hash
-      class HashBased
-        include Operator, Operator::Relational, Operator::Unary
-  
-        def initialize(by_key, allbut, summarization)
-          @by_key, @allbut, @summarization = by_key, allbut, summarization
-        end
-
-        protected
-        
-        def _each
-          index = Hash.new{|h,k| @summarization.least}
-          each_input_tuple do |tuple|
-            key, rest = @by_key.split_tuple(tuple, @allbut)
-            index[key] = @summarization.happens(index[key], tuple)
-          end
-          index.each_pair do |key,aggs|
-            yield key.merge(@summarization.finalize(aggs))
-          end
-        end
-      
-      end
-        
-      protected 
-      
-      def longexpr
+      # (see Operator#each)
+      def each(&block)
         if @allbut
-          chain HashBased.new(@by, @allbut, @summarization),
-                datasets
+          op = Engine::Summarize::Hash.new(input, by, summarization, allbut)
+          op.each(&block)
         else
-          chain SortBased.new(@by, @allbut, @summarization),
-                Operator::NonRelational::Sort.new(@by.to_ordering),
-                datasets
+          op = Engine::Sort.new(input, by.to_ordering)
+          op = Engine::Summarize::Cesure.new(op, by, summarization, allbut)
+          op.each(&block)
         end
       end
   
