6ece103e41b25ed16e300207de6cdd9e2eb1ec18
Bernard Lambeau <blambeau@gmail.com>
2011-12-09 09:18:45 +0100

    Rank operator uses Engine::Rank

--- lib/alf/operator/relational/rank.rb
+++ lib/alf/operator/relational/rank.rb
@@ -1,55 +1,20 @@
 module Alf
   module Operator::Relational
     class Rank < Alf::Operator()
-      include Operator::Relational, Operator::Shortcut, Operator::Unary
-  
+      include Relational, Unary
+
       signature do |s|
         s.argument :order, Ordering, []
         s.argument :as, AttrName, :rank
       end
-      
-      class SortBased
-        include Operator, Operator::Cesure
-        
-        def initialize(order, as)
-          @by_key = AttrList.coerce(order)
-          @as = as
-        end
-        
-        protected
-        
-        # (see Operator::Cesure#project)
-        def project(tuple)
-          @by_key.project_tuple(tuple, false)
-        end
-    
-        # (see Operator::Cesure#start_cesure)
-        def start_cesure(key, receiver)
-          @rank ||= 0
-          @last_block = 0
-        end
-  
-        # (see Operator::Cesure#accumulate_cesure)
-        def accumulate_cesure(tuple, receiver)
-          receiver.call tuple.merge(@as => @rank)
-          @last_block += 1
-        end
-        
-        # (see Operator::Cesure#flush_cesure)
-        def flush_cesure(key, receiver)
-          @rank += @last_block
-        end
-  
-      end # class SortBased
-  
-      protected
-      
-      def longexpr
-        chain SortBased.new(@order, @as),
-              Operator::NonRelational::Sort.new(@order),
-              datasets
+
+      # (see Operator#each)
+      def each(&block)
+        op = Engine::Sort.new(input, order)
+        op = Engine::Rank::Cesure.new(op, order, as)
+        op.each(&block)
       end 
-  
+
     end # class Rank
   end # module Operator::Relational
 end # module Alf
