932d2d7cee010f7bbd43d2c914e4dac775757803
Bernard Lambeau <blambeau@gmail.com>
2012-06-20 13:52:57 +0200

    Split Alf vs. Alf#lispy in integration tests.

--- spec/integration/test_alf.rb
+++ spec/integration/test_alf.rb
@@ -1,34 +1,8 @@
 require 'spec_helper'
 describe Alf do
   
-  let(:lispy){ Alf.lispy(Alf::Environment.examples) } 
-  
-  let(:expected){[
-    {:status => 20, :sid=>"S1", :name=>"Smith", :city=>"London"},
-    {:status => 20, :sid=>"S4", :name=>"Clark", :city=>"London"}
-  ]}
-  
-  it "should have a version number" do
+  it "has a version number" do
     Alf.const_defined?(:VERSION).should be_true
-  end
-  
-  it "should allow running a commandline like command" do
-    op = lispy.run(['restrict', 'suppliers', '--', "city == 'London'"])
-    op.to_a.should == expected
-  end
-  
-  it "should allow compiling lispy expressions" do
-    lispy.compile {
-      (restrict :suppliers, lambda{ city == 'London'})
-    }.to_a.should == expected
-  end
-  
-  it "should allow evaluating lispy expressions" do
-    rel = lispy.evaluate {
-      (restrict :suppliers, lambda{ city == 'London'})
-    }
-    rel.should be_a(Alf::Relation)
-    rel.should eq(Alf::Relation.coerce(expected))
   end
   
 end
