2de2fb5a60a8d760f1932b9522078c89e1e37a3b
Bernard Lambeau <blambeau@gmail.com>
2012-07-25 17:49:07 +0200

    Add Project#keys

--- lib/alf/operator/relational/project.rb
+++ lib/alf/operator/relational/project.rb
@@ -13,6 +13,16 @@ module Alf
           @heading ||= operand.heading.project(attributes, allbut)
         end
 
+        def keys
+          @keys ||= begin
+            keys = operand.keys
+            keys = keys.map{|k| k.project(attributes, allbut) }
+            keys = keys.reject!{|k| k.empty? }
+            keys = [ heading.to_attr_list.project(attributes, allbut) ] if keys.empty?
+            keys
+          end
+        end
+
       end # class Project
     end # module Relational
   end # module Operator
