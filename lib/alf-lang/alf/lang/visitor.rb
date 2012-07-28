76186ad4f1d5d0548b5cc90c44853a4c116d5d4d
Bernard Lambeau <blambeau@gmail.com>
2012-07-28 12:30:05 +0200

    Don't apply visit outside of Alf::Operator for now

--- lib/alf-lang/alf/lang/visitor.rb
+++ lib/alf-lang/alf/lang/visitor.rb
@@ -5,10 +5,14 @@ module Alf
     private
 
       def to_method_name(expr, prefix = "on_")
-        name = Tools.ruby_case(Tools.class_name(expr.class))
-        meth = :"#{prefix}#{name}"
-        meth = :"#{prefix}missing" unless respond_to?(meth)
-        meth
+        if expr.class.name.to_s =~ /^Alf::Operator/
+          name = Tools.ruby_case(Tools.class_name(expr.class))
+          meth = :"#{prefix}#{name}"
+          meth = :"#{prefix}missing" unless respond_to?(meth)
+          meth
+        else
+          :on_missing
+        end
       end
 
     end # module Visitor
