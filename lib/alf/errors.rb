229f2161d92edbe0d7065f4b3bb63e2574153cc4
Bernard Lambeau <blambeau@gmail.com>
2012-10-10 09:07:25 +0200

    Remove obsolete error class.

--- lib/alf/errors.rb
+++ lib/alf/errors.rb
@@ -2,7 +2,6 @@ module Alf
   class Error < StandardError; end
   class UnboundError < StandardError; end
   class NotSupportedError < Error; end
-  class NoSuchSchemaError < Error; end
   class NoSuchRelvarError < Error; end
   class NoSuchTupleError < Error; end
   class FactAssertionError < Error; end
