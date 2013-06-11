038102f7a4e16ae17f0946af2a28e86675399404
Bernard Lambeau <blambeau@gmail.com>
2013-06-11 09:17:55 +0200

    Fix test for 1.9.2

--- spec/unit/alf-io/renderer/test_yaml.rb
+++ spec/unit/alf-io/renderer/test_yaml.rb
@@ -11,15 +11,9 @@ module Alf
         subject{ YAML.new(input).execute("") }
 
         let(:input){ Relation(id: [1, 2]) }
-        let(:expected){
-          "---\n" <<
-          "- :id: 1\n" <<
-          "- :id: 2\n" <<
-          "\n"
-        }
 
         it 'outputs as expected' do
-          subject.should eq(expected)
+          ::YAML.load(subject).should eq([{id: 1}, {id: 2}])
         end
       end
 
