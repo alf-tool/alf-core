d5c1c793afd12d2928460af6a641e31c8db430ff
Bernard Lambeau <blambeau@gmail.com>
2013-09-10 19:40:12 +0200

    Add a pending test that shows the semantic issue with Ordering's selectors on RVAs.

--- spec/unit/alf-types/ordering/test_sorter.rb
+++ spec/unit/alf-types/ordering/test_sorter.rb
@@ -44,5 +44,27 @@ module Alf
       end
     end
 
+    context 'when RVA attributes are involved' do
+      let(:ordering){ Ordering.coerce([[:a, :asc], [[:b, :x], :desc]]) }
+
+      let(:tuples){
+        [{a: 2, b: [{x: 1}]},
+         {a: 7, b: [{x: 1}]},
+         {a: 2, b: [{x: 2}]}]
+      }
+
+      let(:expected){
+        [{a: 2, b: [{x: 1}]},
+         {a: 2, b: [{x: 2}]},
+         {a: 7, b: [{x: 1}]}]
+      }
+
+      it 'should sort correctly' do
+        pending{
+          tuples.sort(&subject).should eq(expected)
+        }
+      end
+    end
+
   end # Ordering
 end # Alf
