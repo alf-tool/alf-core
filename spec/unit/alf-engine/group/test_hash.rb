a4dd1b10cbc47ff514126128fc26d01d3f89872b
Bernard Lambeau <blambeau@gmail.com>
2014-04-30 14:49:21 +0200

    Test coverage on some nasty NULL cases.

--- spec/unit/alf-engine/group/test_hash.rb
+++ spec/unit/alf-engine/group/test_hash.rb
@@ -3,25 +3,46 @@ module Alf
   module Engine
     describe Group::Hash do
 
-      let(:operand){[
-        {name: "Smith", city: "London"},
-        {name: "Jones", city: "Paris"},
-        {name: "Blake", city: "London"},
-      ]}
+      context 'the normal case' do
+        let(:operand){[
+          {name: "Smith", city: "London"},
+          {name: "Jones", city: "Paris"},
+          {name: "Blake", city: "London"},
+        ]}
 
-      let(:expected){[
-        { city: "London", suppliers: Relation(name: ["Smith", "Blake"]) },
-        { city: "Paris",  suppliers: Relation(name: ["Jones"])          },
-      ].to_set}
+        let(:expected){[
+          { city: "London", suppliers: Relation(name: ["Smith", "Blake"]) },
+          { city: "Paris",  suppliers: Relation(name: ["Jones"])          },
+        ].to_set}
 
-      it 'should group specified attributes' do
-        op = Group::Hash.new(operand, AttrList[:name], :suppliers, false)
-        op.to_set.should eq(expected)
+        it 'should group specified attributes' do
+          op = Group::Hash.new(operand, AttrList[:name], :suppliers, false)
+          op.to_set.should eq(expected)
+        end
+
+        it 'should allow specifying allbut grouping' do
+          op = Group::Hash.new(operand, AttrList[:city], :suppliers, true)
+          op.to_set.should eq(expected)
+        end
       end
 
-      it 'should allow specifying allbut grouping' do
-        op = Group::Hash.new(operand, AttrList[:city], :suppliers, true)
-        op.to_set.should eq(expected)
+      context 'in presence of null values' do
+        let(:operand){[
+          {name: "Smith", city: "London" },
+          {name: nil,     city: "Paris"  },
+          {name: "Blake", city: "London" },
+        ]}
+
+        let(:expected){[
+          { city: "London", suppliers: Relation(name: ["Smith", "Blake"]) },
+          { city: "Paris",  suppliers: Relation(name: [nil])              },
+        ].to_set}
+
+        it 'should not fail' do
+          op = Group::Hash.new(operand, AttrList[:name], :suppliers, false)
+          op.to_relation
+          op.to_set.should eq(expected)
+        end
       end
 
     end
--- spec/unit/alf-relation/tuple/test_triple_equal.rb
+++ spec/unit/alf-relation/tuple/test_triple_equal.rb
@@ -2,16 +2,44 @@ require 'spec_helper'
 module Alf
   describe Tuple, "===" do
 
-    let(:type){ Tuple[price: Float] }
-
     subject{ type === tuple }
 
     context 'when applied to a most specific tuple than declared' do
-      let(:tuple){ Tuple[price: Numeric].new(price: 12.0) }
+      let(:type){
+        Tuple[price: Float]
+      }
+
+      let(:tuple){
+        type.new(price: 12.0)
+      }
+
+      it{ should be_true }
+    end
+
+    context 'when applied to a most specific tuple than declared' do
+      let(:type){
+        Tuple[price: Float]
+      }
+
+      let(:tuple){
+        Tuple[price: Numeric].new(price: 12.0)
+      }
 
       it{
         pending("most-specific types not implemented"){ should be_true }
       }
     end
+
+    context 'when nil is used' do
+      let(:type){
+        Tuple[default: String]
+      }
+      let(:tuple){
+        type.new(:default => nil)
+      }
+
+      it{ should be_true }
+    end
+
   end
 end
